20a09589a59c0942c4f76324890019d904b574f2
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-13 23:55:33 +0200

    initialized nanogram model

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/api/NanogramModel.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/api/NanogramModel.java
@@ -1,5 +1,19 @@
 package it.unibo.mparty.model.minigames.nanogram.api;
 
-public interface NanogramModel{
-    
+import java.util.*;
+
+public interface NanogramModel {
+    void initializeGameGrid(int size);
+
+    String getCellState(int row, int column);
+
+    void setCellState(int row, int column, String state);
+
+    List<String> getRowHints();
+
+    List<String> getColumnHints();
+
+    boolean isMoveValid(int row, int column, String move);
+
+    boolean isGameComplete();
 }
--- src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/NanogramModelImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/NanogramModelImpl.java
@@ -1,7 +1,51 @@
 package it.unibo.mparty.model.minigames.nanogram.impl;
 
+import java.util.List;
+
 import it.unibo.mparty.model.minigames.nanogram.api.NanogramModel;
 
 public class NanogramModelImpl implements NanogramModel{
+
+    @Override
+    public void initializeGameGrid(int size) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initializeGameGrid'");
+    }
+
+    @Override
+    public String getCellState(int row, int column) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCellState'");
+    }
+
+    @Override
+    public void setCellState(int row, int column, String state) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCellState'");
+    }
+
+    @Override
+    public List<String> getRowHints() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRowHints'");
+    }
+
+    @Override
+    public List<String> getColumnHints() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getColumnHints'");
+    }
+
+    @Override
+    public boolean isMoveValid(int row, int column, String move) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMoveValid'");
+    }
+
+    @Override
+    public boolean isGameComplete() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isGameComplete'");
+    }
     
 }
