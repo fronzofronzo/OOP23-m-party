1b89be33817999b94c3ba933591d9777c18173e7
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-06-14 17:48:28 +0200

    Create files of slots interfaces and implementetion

---
+++ src/main/java/it/unibo/mparty/model/gameBoard/api/GameBoard.java
@@ -0,0 +1,8 @@
+package it.unibo.mparty.model.gameBoard.api;
+
+public interface GameBoard {
+
+    //come input potrebbe avere un numero di caselle o una difficoltà selezionata
+    void createGameBoard();
+
+}
---
+++ src/main/java/it/unibo/mparty/model/gameBoard/impl/GameBoardImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.mparty.model.gameBoard.impl;
+
+import it.unibo.mparty.model.gameBoard.api.GameBoard;
+
+public class GameBoardImpl implements GameBoard {
+
+    private static final int N_SLOTS = 30;
+
+
+    @Override
+    public void createGameBoard() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGameBoard'");
+    }
+    
+
+}
