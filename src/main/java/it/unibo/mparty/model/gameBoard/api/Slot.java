af0d77d31fa18b60fca01b8b9ebb184eea23a968
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-06-15 17:50:18 +0200

    Created and implemented enum Direction.java and implemented Slot interface

--- src/main/java/it/unibo/mparty/model/gameBoard/api/Slot.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/api/Slot.java
@@ -1,6 +1,21 @@
 package it.unibo.mparty.model.gameBoard.api;
 
+import java.util.Map;
+import java.util.Optional;
+
+import it.unibo.mparty.model.gameBoard.util.Coordinate;
+import it.unibo.mparty.model.gameBoard.util.Direction;
+import it.unibo.mparty.model.gameBoard.util.SlotType;
+
 public interface Slot {
 
-    
+    public Coordinate getCoordinate();
+
+    public void addConnection();
+
+    public Optional<Slot> getConnection(Direction dir);
+
+    public Map<Direction, Slot> getConnections();
+
+    public SlotType getSlotType();    
 }
