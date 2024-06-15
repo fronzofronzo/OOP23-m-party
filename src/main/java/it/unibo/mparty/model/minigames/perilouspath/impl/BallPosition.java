6f8517531bbe8c30bee75b71189087e7b2dc4c6e
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-15 18:20:30 +0200

    created BallPosition and BombPosition, the implementations of AbstractPostion, BombPosition still empty tho

---
+++ src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/BallPosition.java
@@ -0,0 +1,21 @@
+package it.unibo.mparty.model.minigames.perilouspath.impl;
+
+import java.util.List;
+
+import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
+
+public class BallPosition extends AbstractPosition{
+
+
+    public BallPosition(int x, int y, int size) {
+        super(x, y, size);
+        
+    }
+
+    @Override
+    public boolean isSafe(List<AbstractPosition> list) {
+        return this.getY() == 0 || this.getY() == this.getSize();
+    }
+
+    
+}
