43936aa899295c1b54046c9b45bc2412488c1181
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-17 16:59:13 +0200

    Added character and position method to  PlayerBuilder interface

--- src/main/java/it/unibo/mparty/model/player/api/PlayerBuilder.java
+++ src/main/java/it/unibo/mparty/model/player/api/PlayerBuilder.java
@@ -1,11 +1,28 @@
 package it.unibo.mparty.model.player.api;
 
+import it.unibo.mparty.model.player.Position;
+import it.unibo.mparty.model.player.impl.Character;
+
 public interface PlayerBuilder {
 
     /**
-     * Sets the username of the player
-     * @param username of the player
+     * Sets the username of player
+     * @param username of player
      * @return current builder
      */
     PlayerBuilder username(String username);
+
+    /**
+     * Sets the character chosen by the player
+     * @param character chosen for the player
+     * @return current builder
+     */
+    PlayerBuilder character(Character character);
+
+    /**
+     * Sets the initial position of player
+     * @param position of player at the start of the game
+     * @return current builder
+     */
+    PlayerBuilder position(Position position);
 }
