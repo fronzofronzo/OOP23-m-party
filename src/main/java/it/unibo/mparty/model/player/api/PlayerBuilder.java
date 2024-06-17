00dbc34b40e8181bf83fd5b9db0405a6c3778fc7
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-17 16:54:35 +0200

    Added username method to  PlayerBuilder interface

--- src/main/java/it/unibo/mparty/model/player/api/PlayerBuilder.java
+++ src/main/java/it/unibo/mparty/model/player/api/PlayerBuilder.java
@@ -2,4 +2,10 @@ package it.unibo.mparty.model.player.api;
 
 public interface PlayerBuilder {
 
+    /**
+     * Sets the username of the player
+     * @param username of the player
+     * @return current builder
+     */
+    PlayerBuilder username(String username);
 }
