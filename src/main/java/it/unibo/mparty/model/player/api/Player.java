a8f18ce5e4f2168e8283419b0886d285d27b8e29
Gabriele Fronzoni <gabriele.fronzoni@studio.unibo.it>
2024-06-17 14:33:59 +0200

    Implemented methods in Player interface and created Character enumeration

---
+++ src/main/java/it/unibo/mparty/model/player/api/Player.java
@@ -0,0 +1,36 @@
+package it.unibo.mparty.model.player.api;
+
+/**
+ *  This interface models a Player
+ */
+public interface Player {
+
+    /**
+     * Sets the username of the player
+     *
+     * @param username is the username chosen
+     */
+    void setUsername(String username);
+
+    /**
+     * Get the username of a certain player
+     *
+     * @return the username of the player
+     */
+    String getUsername();
+
+    /**
+     * Sets the username of the player
+     *
+     * @param username is the username chosen
+     */
+    void setCharacter(String username);
+
+    /**
+     * Get the username of a certain player
+     *
+     * @return the username of the player
+     */
+    String getUsername();
+
+}
---
+++ src/main/java/it/unibo/mparty/model/player/impl/Character.java
@@ -0,0 +1,4 @@
+package it.unibo.mparty.model.player.impl;
+
+public enum Character {
+}
