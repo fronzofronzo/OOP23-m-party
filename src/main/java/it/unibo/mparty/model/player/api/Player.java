5fca3b92338566f9f86e1969bef5c0a41b888ff9
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-17 18:28:05 +0200

    Added getItem method to Player interface

--- src/main/java/it/unibo/mparty/model/player/api/Player.java
+++ src/main/java/it/unibo/mparty/model/player/api/Player.java
@@ -70,4 +70,11 @@ public interface Player {
      */
     void removeStar();
 
+    /**
+     * Get a certain item from player items collection
+     * @param i index of the selected item
+     * @return the selected item
+     */
+    Item getItem(int i);
+
 }
