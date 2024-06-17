2bd03ebe1cd198be543fe2da925f78c078f8ad3e
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-17 17:10:31 +0200

    Initialized PlayerBuilderImplementation class

---
+++ src/main/java/it/unibo/mparty/model/player/impl/PlayerBuilderImplementation.java
@@ -0,0 +1,27 @@
+package it.unibo.mparty.model.player.impl;
+
+import it.unibo.mparty.model.player.Position;
+import it.unibo.mparty.model.player.api.Player;
+import it.unibo.mparty.model.player.api.PlayerBuilder;
+
+public class PlayerBuilderImplementation implements PlayerBuilder {
+    @Override
+    public PlayerBuilder username(String username) {
+        return null;
+    }
+
+    @Override
+    public PlayerBuilder character(Character character) {
+        return null;
+    }
+
+    @Override
+    public PlayerBuilder position(Position position) {
+        return null;
+    }
+
+    @Override
+    public Player buildPlayer() {
+        return null;
+    }
+}
