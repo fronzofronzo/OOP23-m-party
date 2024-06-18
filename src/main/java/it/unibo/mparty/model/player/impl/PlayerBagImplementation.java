63fc194a5905410af8fcba6494a198ac54925c83
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-18 09:41:31 +0200

    Initialized PlayerBagImplementation class

--- src/main/java/it/unibo/mparty/model/player/impl/PlayerBagImplementation.java
+++ src/main/java/it/unibo/mparty/model/player/impl/PlayerBagImplementation.java
@@ -1,4 +1,22 @@
 package it.unibo.mparty.model.player.impl;
 
-public class PlayerBagImplementation {
+import it.unibo.mparty.model.player.api.PlayerBag;
+
+public class PlayerBagImplementation implements PlayerBag {
+
+
+    @Override
+    public Item getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public void addItem(Item item) {
+
+    }
+
+    @Override
+    public void removeItem(int i) {
+
+    }
 }
