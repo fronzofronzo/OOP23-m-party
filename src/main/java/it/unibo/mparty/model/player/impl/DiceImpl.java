8d6cbb673852b818af7f3443535269b12892dcb8
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-19 17:28:10 +0200

    Implemented costant numbers in DiceImpl class

--- src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
+++ src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
@@ -4,6 +4,10 @@ import it.unibo.mparty.model.player.api.Dice;
 
 public class DiceImpl implements Dice {
 
+    private static final int ATTEMPTS = 1;
+    private static final int MAX_NUMBER = 6;
+    private static final int MIN_NUMBER = 1;
+
     private int numberOfAttempts = 1;
     private int maxNumber = 6;
     private int minNumber = 1;
