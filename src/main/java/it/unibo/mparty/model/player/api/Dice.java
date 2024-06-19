ac325176244dcc6ad09e43b369824d2ed0a9a62d
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-19 16:55:36 +0200

    Added 3 methods in Dice Interface

--- src/main/java/it/unibo/mparty/model/player/api/Dice.java
+++ src/main/java/it/unibo/mparty/model/player/api/Dice.java
@@ -1,4 +1,24 @@
 package it.unibo.mparty.model.player.api;
 
 public interface Dice {
+
+    /**
+     * Sets the number of attempts that can be done when the player rolls
+     * the dice
+     * @param num of attempts
+     */
+    void setNumberOfAttempts(int num);
+
+    /**
+     * Sets max number that player can make when the player rolls dice
+     * @param num to set as max
+     */
+    void setMaxNumber(int num);
+
+    /**
+     * Sets minimum number that player can make when the player rolls dice
+     * @param num to set as minimum
+     */
+    void setMinNumber(int num);
+
 }
