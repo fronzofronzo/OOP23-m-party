7bf8052b89d40fbc6264992f1f3a7b9a8ae3e25a
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-19 15:32:59 +0200

    added setters and getters for bombs and balls

--- src/main/java/it/unibo/mparty/model/minigames/perilouspath/api/PerilousPath.java
+++ src/main/java/it/unibo/mparty/model/minigames/perilouspath/api/PerilousPath.java
@@ -1,8 +1,19 @@
 package it.unibo.mparty.model.minigames.perilouspath.api;
 
+import java.util.List;
+
 public interface PerilousPath {
     public enum type{
         BOMB,BALL,PATH
     }
+
+    
+    public void setBombs();
+
+    public void setBalls();
+
+    public List<AbstractPosition> getBombs();
+
+    public List<AbstractPosition> getBalls();
     
 }
