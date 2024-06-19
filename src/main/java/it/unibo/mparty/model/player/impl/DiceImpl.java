fad89ab929a253e63d6d32f0d73147fdd01c3153
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-19 17:20:24 +0200

    Implemented private fields in DiceImpl class

--- src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
+++ src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
@@ -3,4 +3,28 @@ package it.unibo.mparty.model.player.impl;
 import it.unibo.mparty.model.player.api.Dice;
 
 public class DiceImpl implements Dice {
+
+    final int numberOfAttempts = 1;
+    final int maxNumber = 6;
+    final int minNumber = 1;
+
+    @Override
+    public void setNumberOfAttempts(int num) {
+
+    }
+
+    @Override
+    public void setMaxNumber(int num) {
+
+    }
+
+    @Override
+    public void setMinNumber(int num) {
+
+    }
+
+    @Override
+    public int generateNumber() {
+        return 0;
+    }
 }
