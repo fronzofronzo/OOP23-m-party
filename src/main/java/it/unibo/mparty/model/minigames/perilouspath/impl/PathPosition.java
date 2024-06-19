1e68755d1b1cbf4eac8bf78398243ba85a6a3197
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-19 18:52:22 +0200

    added the comments for the PathPosition class

--- src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/PathPosition.java
+++ src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/PathPosition.java
@@ -6,10 +6,17 @@ import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
 
 public class PathPosition extends AbstractPosition{
 
+    /*
+     * constructor which calls the constructor of AbstractPosition
+     */
     public PathPosition(int x, int y, int size) {
         super(x, y, size);
     }
 
+    /**
+     * implementation of the abstract method isSafe(),
+     * in this case a position can be safe only if it is near at least to another position
+     */
     @Override
     public boolean isSafe(List<AbstractPosition> list) {
         return list.size() == 0 ? true : list.stream().anyMatch(p -> this.inOrizzontal(p) || this.inOrizzontal(p));
