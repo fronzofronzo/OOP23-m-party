151713dfed04c98244bbb49b9962025461341252
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-19 18:25:20 +0200

    Added firstClick method to memoryCardModel interface

--- src/main/java/it/unibo/mparty/model/minigames/memoryCard/api/memoryCardModel.java
+++ src/main/java/it/unibo/mparty/model/minigames/memoryCard/api/memoryCardModel.java
@@ -1,4 +1,12 @@
 package it.unibo.mparty.model.minigames.memoryCard.api;
 
+import it.unibo.mparty.model.player.Position;
+
 public interface memoryCardModel {
+
+    /**
+     * Select the first card of the pair
+     * @param card it's the first card selected
+     */
+    void firstClick(Position card);
 }
