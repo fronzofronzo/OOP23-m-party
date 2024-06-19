dc05620244507db405fc290bd27b9e1379e73dff
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-19 16:08:06 +0200

    Initialized Dice Interface and class Implementation

---
+++ src/main/java/it/unibo/mparty/model/player/api/Dice.java
@@ -0,0 +1,4 @@
+package it.unibo.mparty.model.player.api;
+
+public interface Dice {
+}
---
+++ src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
@@ -0,0 +1,6 @@
+package it.unibo.mparty.model.player.impl;
+
+import it.unibo.mparty.model.player.api.Dice;
+
+public class DiceImpl implements Dice {
+}
