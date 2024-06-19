88392e3a1d88f78f571ffb4ee3b69e533913c133
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-19 14:48:43 +0200

    changed the method isSafe() in BombPosition

--- src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/BombPosition.java
+++ src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/BombPosition.java
@@ -1,10 +1,13 @@
 package it.unibo.mparty.model.minigames.perilouspath.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
 
 public class BombPosition extends AbstractPosition{
+
+    final private static int NUMBER = 2;
 
     public BombPosition(int x, int y, int size) {
         super(x, y, size);
@@ -15,7 +18,7 @@ public class BombPosition extends AbstractPosition{
         if(list.size() == 0){
             return true;
         }
-        return list.stream().allMatch(b -> !this.adjacent(b));
+        return list.stream().filter(b -> this.adjacent(this)).collect(Collectors.toList()).size() <= NUMBER;
     }
 
     
