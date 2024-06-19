a60210e0538f3257f6686e2a0030db6787842905
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-19 21:00:02 +0200

    Implemented private fields in MemoryCardModelImpl

--- src/main/java/it/unibo/mparty/model/minigames/memoryCard/impl/MemoryCardModelImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/memoryCard/impl/MemoryCardModelImpl.java
@@ -3,8 +3,14 @@ package it.unibo.mparty.model.minigames.memoryCard.impl;
 import it.unibo.mparty.model.minigames.memoryCard.api.MemoryCardModel;
 import it.unibo.mparty.model.player.Position;
 
+import java.util.Map;
+import java.util.Set;
+
 public class MemoryCardModelImpl implements MemoryCardModel {
 
+    private final Map<Position, CardType> cards;
+    private final Set<CardType> guessed;
+    private int mistakesNumber;
 
     @Override
     public void firstClick(Position card) {
