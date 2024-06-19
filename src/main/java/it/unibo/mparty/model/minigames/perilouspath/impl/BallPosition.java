3918f7a4e006b33292769c0dcccb4f6de94d28a9
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-19 18:45:00 +0200

    added the comments for the BallPosition class

--- src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/BallPosition.java
+++ src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/BallPosition.java
@@ -3,17 +3,24 @@ package it.unibo.mparty.model.minigames.perilouspath.impl;
 import java.util.List;
 
 import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
-
+/**
+ * Implementation of AbstractPosition for balls, which has a special isSafe() politics
+ */
 public class BallPosition extends AbstractPosition{
 
     private static final int MINUS = 1;
 
-
+    /*
+     * constructor which calls the constructor of AbstractPosition
+     */
     public BallPosition(int x, int y, int size) {
-        super(x, y, size);
-        
+        super(x, y, size);   
     }
 
+    /**
+     * implementation of the abstract method isSafe(), 
+     * in this case the position is safe only if it is on the first or the last column
+     */
     @Override
     public boolean isSafe(List<AbstractPosition> list) {
         return this.getY() == 0 || this.getY() == this.getSize() - MINUS;
