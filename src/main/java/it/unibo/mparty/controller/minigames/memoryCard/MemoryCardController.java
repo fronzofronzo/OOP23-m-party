6e8ea6eafca2e910620b5deaed32aac31aa69076
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-20 17:34:37 +0200

    added selectFirst and selectSecond methods to MemoryCardController interface

--- src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
@@ -1,4 +1,15 @@
 package it.unibo.mparty.controller.minigames.memoryCard;
 
 public interface MemoryCardController {
+
+    /**
+     * It selects the first card of the pair that the player is trying to guess
+     */
+    void selectFirst();
+
+    /**
+     * It selects the second card of the pair that the player
+     * is trying to guess
+     */
+    void selectSecond();
 }
--- src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardControllerImpl.java
@@ -1,4 +1,14 @@
 package it.unibo.mparty.controller.minigames.memoryCard;
 
 public class MemoryCardControllerImpl implements MemoryCardController{
+
+    @Override
+    public void selectFirst() {
+
+    }
+
+    @Override
+    public void selectSecond() {
+
+    }
 }
