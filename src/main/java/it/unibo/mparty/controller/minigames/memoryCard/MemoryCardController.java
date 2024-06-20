67a0893673bae4524659fe246560c11985a73507
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-20 17:41:47 +0200

    initialized and implemented constructor in MemoryCardControllerImpl

--- src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
@@ -12,4 +12,6 @@ public interface MemoryCardController {
      * is trying to guess
      */
     void selectSecond();
+
+
 }
--- src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardControllerImpl.java
@@ -1,6 +1,18 @@
 package it.unibo.mparty.controller.minigames.memoryCard;
 
+import it.unibo.mparty.model.minigames.memoryCard.api.MemoryCardModel;
+import it.unibo.mparty.model.minigames.memoryCard.impl.MemoryCardModelImpl;
+import it.unibo.mparty.view.minigames.memoryCard.MemoryCardView;
+
 public class MemoryCardControllerImpl implements MemoryCardController{
+
+    private final MemoryCardModel model;
+    private final MemoryCardView view;
+
+    public MemoryCardControllerImpl(MemoryCardView view){
+        this.model = new MemoryCardModelImpl();
+        this.view = view;
+    }
 
     @Override
     public void selectFirst() {
