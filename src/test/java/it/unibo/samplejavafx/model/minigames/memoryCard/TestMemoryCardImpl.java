6ee48f95eb54f858b170be714cee402f73a74b81
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-20 10:19:22 +0200

    initialized TestMemoryCardImpl class

---
+++ src/test/java/it/unibo/samplejavafx/model/minigames/memoryCard/TestMemoryCardImpl.java
@@ -0,0 +1,10 @@
+package it.unibo.samplejavafx.model.minigames.memoryCard;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestMemoryCardImpl {
+
+
+}
