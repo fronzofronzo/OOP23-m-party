667e314a62072cae02be0c5c9cdfc9fb9d97d216
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-20 12:07:38 +0200

    added the method getSize() and changed the method isSafe() in PerilousPathImpl class

--- src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/PerilousPathImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/perilouspath/impl/PerilousPathImpl.java
@@ -30,13 +30,13 @@ public class PerilousPathImpl implements PerilousPath{
 
     @Override
     public void setBombs() {
-        IntStream.iterate(0, i -> i + 1).limit(NUM_BOMBS).forEach(b -> this.bombs.get().add(setNewPosition()));
+        IntStream.iterate(0, i -> i + 1).limit(NUM_BOMBS).forEach(b -> this.bombs.get().add(setNewBombPosition()));
     }
 
     @Override
     public void setBalls() {
-        this.balls.get().add(new BallPosition(this.random.nextInt(this.size - 1),0,this.size));
-        this.balls.get().add(new BallPosition(this.random.nextInt(this.size - 1),this.size - 1,this.size));
+        this.balls.get().add(this.setNewBallPosition(0));
+        this.balls.get().add(this.setNewBallPosition(this.getSize() - 1));
     }
 
     @Override
@@ -57,7 +57,7 @@ public class PerilousPathImpl implements PerilousPath{
         if(this.balls.get().contains(p)){
             return Type.BALL;
         }
-        if(p.isSafe(this.path.get())){
+        if(p.isSafe(this.path.get(),this.getBalls())){
             this.path.get().add(p);
             return Type.PATH;
         }
@@ -67,7 +67,7 @@ public class PerilousPathImpl implements PerilousPath{
     @Override
     public boolean isOver() {
         var p1 = this.path.get().get(0);
-        var p2 = this.path.get().get(this.size - 1);
+        var p2 = this.path.get().get(this.getSize() - 1);
         if(p1.adjacent(this.balls.get().get(0)) && p2.adjacent(this.balls.get().get(1))){
             return true;
         }
@@ -79,16 +79,34 @@ public class PerilousPathImpl implements PerilousPath{
         return Collections.unmodifiableList(this.path.get());
     }
 
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
     /**
      * a private method for getting always a new BombPosition in a pseudo-random manner
      * @return a new BombPosition which is safe
      */
-    private AbstractPosition setNewPosition(){
+    private AbstractPosition setNewBombPosition(){
         BombPosition b;
         do{
-            b = new BombPosition(random.nextInt(this.size - 1), random.nextInt(this.size - 1), this.size);
-        }while (!b.isSafe(this.getBombs()));
+            b = new BombPosition(random.nextInt(this.getSize() - 1), random.nextInt(this.getSize()- 1), this.getSize());
+        }while (!b.isSafe(this.getBombs(),List.of()));
         return b;
-
     }
+
+    /**
+     * a private method for getting always a new BallPosition in a pseudo-random manner
+     * @param y the already setted y position in a generic(x,y) position, meaning that onlye the x position will be random
+     * @return a new BallPosition which is safe
+     */
+    private AbstractPosition setNewBallPosition(int y){
+        BallPosition b;
+        do{
+            b = new BallPosition(this.random.nextInt(this.getSize() - 1), y, this.getSize());
+        }while(!b.isSafe(List.of(), List.of()));
+        return b;
+    }
+
 }
