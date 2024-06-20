ac3d8d41fb7ae23d1b04ee49b4ecab385365645b
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-20 18:07:45 +0200

    deleted selectFirst and selectSecond methods in MemoryCardController

--- src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
@@ -2,16 +2,7 @@ package it.unibo.mparty.controller.minigames.memoryCard;
 
 public interface MemoryCardController {
 
-    /**
-     * It selects the first card of the pair that the player is trying to guess
-     */
-    void selectFirst(int index);
 
-    /**
-     * It selects the second card of the pair that the player
-     * is trying to guess
-     */
-    void selectSecond();
 
 
 }
