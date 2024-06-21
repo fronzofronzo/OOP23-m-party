e0ba085788961dbacabc03ff9e0268d3e088bdc6
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-06-21 18:59:34 +0200

    Created and completed MEDIUM BOARD and its tests

--- src/main/java/it/unibo/mparty/model/gameBoard/boards/MediumGameBoard.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/boards/MediumGameBoard.java
@@ -1,71 +1,86 @@
 package it.unibo.mparty.model.gameBoard.boards;
 
-import java.util.List;
-import java.util.Set;
-
 import it.unibo.mparty.model.gameBoard.impl.AbstractBoardImpl;
 import it.unibo.mparty.model.gameBoard.util.BoardType;
+import it.unibo.mparty.model.gameBoard.util.Pair;
 import it.unibo.mparty.model.gameBoard.util.Position;
+import it.unibo.mparty.model.gameBoard.util.RandomFromSet;
+import it.unibo.mparty.model.gameBoard.util.RandomListGenerator;
 import it.unibo.mparty.model.gameBoard.util.SlotType;
+import java.util.Set;
+import java.util.List;
+import java.util.Collections;
 
 public class MediumGameBoard extends AbstractBoardImpl{
 
-    private static final int WIDTH = 40;
-    private static final int HEIGHT = 30;
-    private static final int INITIAL_X_MEDIUM_BOARD = 5;
-    private static final int INITIAL_Y_MEDIUM_BOARD = 25;
+    private final static BoardType boardType = BoardType.MEDIUM; 
 
+    private static final int WIDTH = 30;
+    private static final int HEIGHT = 20;
+
+    private static final int INITIAL_X_MEDIUM_BOARD = 4;
+    private static final int INITIAL_Y_MEDIUM_BOARD = 16;
+
+    private static final double PROB_PATH = 0.5;
+    private static final double PROB_SINGLEPLAYER = 0.1;
+    private static final double PROB_MALUS = 0.05;
+    private static final double PROB_MULTIPLAYER = 0.1;
+    private static final double PROB_SHOP = 0.1;
+    private static final double PROB_BONUS = 0.15;
+
+    private static final String FILE_PATH = "C:\\Users\\81W1019HIX\\OneDrive\\Desktop\\MARIO PARTY\\OOP23-m-party\\src\\main\\java\\it\\unibo\\mparty\\model\\gameBoard\\file\\MediumBoardGame.txt";
+
+    private static final Set<Pair<SlotType,Double>> RULES = 
+        Set.of(new Pair<>(SlotType.PATH, PROB_PATH),
+               new Pair<>(SlotType.SINGLEPLAYER, PROB_SINGLEPLAYER),
+               new Pair<>(SlotType.MALUS, PROB_MALUS),
+               new Pair<>(SlotType.MULTIPLAYER, PROB_MULTIPLAYER),
+               new Pair<>(SlotType.SHOP, PROB_SHOP),
+               new Pair<>(SlotType.BONUS, PROB_BONUS));
+
+    private static final Set<Position> STARS_POSITIONS = Set.of(new Position(5, 1), 
+                                                                new Position(28, 9),
+                                                                new Position(18, 18));
+           
     public MediumGameBoard() {
         super();
-        //TODO Auto-generated constructor stub
     }
 
     @Override
     public void generateBoard() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generateBoard'");
-    }
-
-    @Override
-    protected SlotType getNewSlotType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNewSlotType'");
-    }
+        this.addSlot(RandomFromSet.get(STARS_POSITIONS), SlotType.ACTIVE_STAR);
+        STARS_POSITIONS.stream().forEach(p -> this.addSlot(p, SlotType.NOT_ACTIVE_STAR));
+        this.addSlot(this.getStrartingPosition(), SlotType.PATH);
+        this.createPathFromFile(FILE_PATH);
+        }
 
     @Override
     public BoardType getBoardType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBoardType'");
+        return boardType;
     }
 
     @Override
     protected Set<Position> setStarsPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setStarsPosition'");
+        return Collections.unmodifiableSet(STARS_POSITIONS);
     }
 
     @Override
     protected int setWidth() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setWidth'");
+        return WIDTH;
     }
 
     @Override
     protected int setHeight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setHeight'");
+        return HEIGHT;
     }
 
     @Override
     protected Position setInitialPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setInitialPosition'");
+        return new Position(INITIAL_X_MEDIUM_BOARD, INITIAL_Y_MEDIUM_BOARD);
     }
 
     @Override
     protected List<SlotType> setAviableSlotType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setAviableSlotType'");
+        return RandomListGenerator.generate(RULES);
     }
-    
 }
--- src/test/java/it/unibo/samplejavafx/gameBoard/TestGameBoard.java
+++ src/test/java/it/unibo/samplejavafx/gameBoard/TestGameBoard.java
@@ -25,33 +25,61 @@ class TestGameBoard{
     private static final int NOT_ACTIVE_STAR_SLOTS_EXPECTED = 2;
     private static final int INITIAL_X_EASY_BOARD = 7;
     private static final int INITIAL_Y_EASY_BOARD = 18;
+    private static final int INITIAL_X_MEDIUM_BOARD = 4;
+    private static final int INITIAL_Y_MEDIUM_BOARD = 16;
 
     
     @SuppressWarnings("unchecked")
-    private final Map<Position,Map<Direction, Position>> dataSetToTestgetNextPositions = 
+    private final Map<Position,Map<Direction, Position>> dataSetToTestgetNextPositionsEasyBoard = 
     Map.of(
         new Position(19, 10), Map.of(Direction.UP, new Position(19, 9)),
         new Position(19, 13), Map.of(Direction.UP, new Position(19, 12),Direction.LEFT, new Position(18, 13)),
         new Position(15,3), Collections.EMPTY_MAP
         );
+    @SuppressWarnings("unchecked")
+    private final Map<Position,Map<Direction, Position>> dataSetToTestgetNextPositionsMediumBoard = 
+        Map.of(
+            new Position(5, 11), Map.of(Direction.UP, new Position(5, 10)),
+            new Position(25, 7), Map.of(Direction.UP, new Position(25, 6),Direction.RIGHT, new Position(26, 7)),
+            new Position(15,2), Collections.EMPTY_MAP
+            );
     
-    private static Board myBoard;
+    private static Board myEasyBoard;
+    private static Board myMediumBoard;
 
     @BeforeAll
     public static void initialise(){
+        //EASY
         SimpleBoardFactory factory = new SimpleBoardFactory();
-        myBoard = factory.createBoard(BoardType.EASY);
-        myBoard.printBoard();
+        myEasyBoard = factory.createBoard(BoardType.EASY);
+        myEasyBoard.printBoard();
+        //MEDIUM
+        myMediumBoard = factory.createBoard(BoardType.MEDIUM);
+        myMediumBoard.printBoard();
     }
 
     @Test 
     public void testNumberStarsSlots(){
-        int count_active_star = (int)myBoard.getBoard()
+        //EASY
+        int count_active_star = (int)myEasyBoard.getBoard()
                                                  .entrySet()
                                                  .stream()
                                                  .filter(entry -> entry.getValue().getSlotType().equals(SlotType.ACTIVE_STAR))
                                                  .count();
-        int count_not_active_star = (int)myBoard.getBoard()
+        int count_not_active_star = (int)myEasyBoard.getBoard()
+                                                     .entrySet()
+                                                     .stream()
+                                                     .filter(entry -> entry.getValue().getSlotType().equals(SlotType.NOT_ACTIVE_STAR))
+                                                     .count();
+        assertEquals(ACTIVE_STAR_SLOTS_EXPECTED, count_active_star);
+        assertEquals(NOT_ACTIVE_STAR_SLOTS_EXPECTED, count_not_active_star);
+        //MEDIUM
+        count_active_star = (int)myMediumBoard.getBoard()
+                                                 .entrySet()
+                                                 .stream()
+                                                 .filter(entry -> entry.getValue().getSlotType().equals(SlotType.ACTIVE_STAR))
+                                                 .count();
+        count_not_active_star = (int)myMediumBoard.getBoard()
                                                      .entrySet()
                                                      .stream()
                                                      .filter(entry -> entry.getValue().getSlotType().equals(SlotType.NOT_ACTIVE_STAR))
@@ -62,21 +90,31 @@ class TestGameBoard{
 
     @Test
     public void testChangeStarPosition() {
-        Position oldStarPosition = myBoard.getStarPosition();
+        //EASY
+        Position oldStarPosition = myEasyBoard.getStarPosition();
         assertNotNull(oldStarPosition);
-        myBoard.changeStarPosition();
-        assertNotEquals(oldStarPosition, myBoard.getStarPosition());
+        myEasyBoard.changeStarPosition();
+        assertNotEquals(oldStarPosition, myEasyBoard.getStarPosition());
+        //MEDIUM
+        oldStarPosition = myMediumBoard.getStarPosition();
+        assertNotNull(oldStarPosition);
+        myMediumBoard.changeStarPosition();
+        assertNotEquals(oldStarPosition, myMediumBoard.getStarPosition());
     }
 
     @Test 
     public void testStartingPosition(){
-        Position expected = null;
-        expected = new Position(INITIAL_X_EASY_BOARD, INITIAL_Y_EASY_BOARD);
-        assertEquals(expected, myBoard.getStrartingPosition());
+        //EASY
+        Position expected = new Position(INITIAL_X_EASY_BOARD, INITIAL_Y_EASY_BOARD);
+        assertEquals(expected, myEasyBoard.getStrartingPosition());
+        //MEDIUM
+        expected = new Position(INITIAL_X_MEDIUM_BOARD, INITIAL_Y_MEDIUM_BOARD);
+        assertEquals(expected, myMediumBoard.getStrartingPosition());
     }
 
     @Test
     public void testGetNextPositions() {
-        this.dataSetToTestgetNextPositions.entrySet().stream().forEach(entry -> assertEquals(entry.getValue(), myBoard.getNextPositions(entry.getKey())));
+        this.dataSetToTestgetNextPositionsEasyBoard.entrySet().stream().forEach(entry -> assertEquals(entry.getValue(), myEasyBoard.getNextPositions(entry.getKey())));
+        this.dataSetToTestgetNextPositionsMediumBoard.entrySet().stream().forEach(entry -> assertEquals(entry.getValue(), myMediumBoard.getNextPositions(entry.getKey())));
     }
 }
