46022137d3526ef1de2de9908b1a3d1bc7d9a0b6
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-22 00:29:03 +0200

    created nanogram board factory

---
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/api/BoardFactory.java
@@ -0,0 +1,6 @@
+package it.unibo.mparty.model.minigames.nanogram.api;
+
+public interface BoardFactory {
+    Board createSimpleBoard(int size, int fillPercentage);
+    Board createHardBoard(int size, int fillPercentage);
+}
---
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardFactoryImpl.java
@@ -0,0 +1,16 @@
+package it.unibo.mparty.model.minigames.nanogram.impl;
+
+import it.unibo.mparty.model.minigames.nanogram.api.Board;
+import it.unibo.mparty.model.minigames.nanogram.api.BoardFactory;
+
+public class BoardFactoryImpl implements BoardFactory {
+    @Override
+    public Board createSimpleBoard(int size, int fillPercentage) {
+        return null;
+    }
+
+    @Override
+    public Board createHardBoard(int size, int fillPercentage) {
+        return null;
+    }
+}
