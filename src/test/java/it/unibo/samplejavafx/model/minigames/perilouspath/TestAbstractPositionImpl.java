820032c37419955397cd5c1ee6eb9f594ff2166f
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-21 16:38:39 +0200

    finished implementing TestAbstractPositionImpl

--- src/test/java/it/unibo/samplejavafx/model/minigames/perilouspath/TestAbstractPositionImpl.java
+++ src/test/java/it/unibo/samplejavafx/model/minigames/perilouspath/TestAbstractPositionImpl.java
@@ -1,12 +1,10 @@
 package it.unibo.samplejavafx.model.minigames.perilouspath;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
