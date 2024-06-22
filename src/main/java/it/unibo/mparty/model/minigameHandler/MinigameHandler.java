52b1eed28a9860bef4ddd3bb72b62f61c73b99f8
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-22 16:57:13 +0200

    added methods to MinigameHandler interface

--- src/main/java/it/unibo/mparty/model/minigameHandler/MinigameHandler.java
+++ src/main/java/it/unibo/mparty/model/minigameHandler/MinigameHandler.java
@@ -1,4 +1,20 @@
 package it.unibo.mparty.model.minigameHandler;
 
 public interface MinigameHandler {
+
+    /**
+     * Start a new minigame
+     */
+    void startMinigame();
+
+    /**
+     * Get the name of the minigame actually played
+     * @return the mini-game's name
+     */
+    String getMinigame();
+
+    /**
+     * Stop current minigame
+     */
+    void stopMinigame();
 }
