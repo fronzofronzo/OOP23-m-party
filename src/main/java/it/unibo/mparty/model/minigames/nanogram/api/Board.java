e8d29ebf0d4256650c811454611ece7d0821fc9e
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-22 12:07:10 +0200

    implemented board

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/api/Board.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/api/Board.java
@@ -1,10 +1,14 @@
 package it.unibo.mparty.model.minigames.nanogram.api;
 
 import it.unibo.mparty.model.minigames.nanogram.util.CellState;
+import it.unibo.mparty.model.minigames.nanogram.util.Pair;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Board {
+    Map<Pair<Integer, Integer>, CellState> getGrid();
+    Map<Pair<Integer, Integer>, CellState> getShowGrid();
     List<Integer> getHints();
     CellState getCellState(int x, int y);
 }
--- src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardImpl.java
@@ -2,17 +2,39 @@ package it.unibo.mparty.model.minigames.nanogram.impl;
 
 import it.unibo.mparty.model.minigames.nanogram.api.Board;
 import it.unibo.mparty.model.minigames.nanogram.util.CellState;
+import it.unibo.mparty.model.minigames.nanogram.util.Pair;
+import javafx.scene.control.Cell;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BoardImpl implements Board {
+    private final Map<Pair<Integer, Integer>, CellState> grid;
+    private final Map<Pair<Integer,Integer>, CellState> showGrid;
+
+    public BoardImpl(Map<Pair<Integer, Integer>, CellState> grid, Map<Pair<Integer, Integer>, CellState> showGrid) {
+        this.grid = grid;
+        this.showGrid = showGrid;
+    }
+
+    @Override
+    public Map<Pair<Integer, Integer>, CellState> getGrid() {
+        return grid;
+    }
+
+    @Override
+    public Map<Pair<Integer, Integer>, CellState> getShowGrid() {
+        return showGrid;
+    }
+
     @Override
     public List<Integer> getHints() {
         return List.of();
     }
 
     @Override
-    public CellState getCellState(int x, int y) {
-        return null;
+    public CellState getCellState(final int x, final int y) {
+        return grid.get(new Pair<>(x,y));
     }
 }
