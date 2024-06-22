84feddb2087f5d4fd593c6871b3fdcc9d5c9f741
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-22 13:46:42 +0200

    Added javadoc and movePlayer method to GameModel interface

--- src/main/java/it/unibo/mparty/model/GameModel.java
+++ src/main/java/it/unibo/mparty/model/GameModel.java
@@ -1,4 +1,12 @@
 package it.unibo.mparty.model;
 
+/**
+ * Interface that models the model of the main game
+ */
 public interface GameModel {
+
+    /**
+     * Moves the current player to a new position
+     */
+    void movePlayer();
 }
