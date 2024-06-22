752169c5ca69af831e6e5b3f3f6763e2697185bc
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-22 13:48:26 +0200

    Added activeMinigame method to GameModel interface

--- src/main/java/it/unibo/mparty/model/GameModel.java
+++ src/main/java/it/unibo/mparty/model/GameModel.java
@@ -9,4 +9,10 @@ public interface GameModel {
      * Moves the current player to a new position
      */
     void movePlayer();
+
+    /**
+     * Get the name of the minigame that is active
+     * @return the name of the minigame
+     */
+    String activeMinigame();
 }
