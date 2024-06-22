ff78fe36623cdaf3d6b140f1567193de54e607a9
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-22 16:14:26 +0200

    Initialized GameModelImpl

--- src/main/java/it/unibo/mparty/model/GameModelImpl.java
+++ src/main/java/it/unibo/mparty/model/GameModelImpl.java
@@ -1,4 +1,45 @@
 package it.unibo.mparty.model;
 
-public class GameModelImpl {
+import it.unibo.mparty.model.player.api.Player;
+
+import java.util.List;
+
+public class GameModelImpl implements GameModel{
+
+    private final List<Player> players;
+    private final GameBoard board;
+    private final Shop shop;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void movePlayer() {
+
+    }
+
+    @Override
+    public String getActiveMinigame() {
+        return "";
+    }
+
+    @Override
+    public boolean isActiveMinigame() {
+        return false;
+    }
+
+    @Override
+    public void nextPlayer() {
+
+    }
+
+    @Override
+    public String getWinner() {
+        return "";
+    }
+
+    @Override
+    public boolean isOver() {
+        return false;
+    }
 }
