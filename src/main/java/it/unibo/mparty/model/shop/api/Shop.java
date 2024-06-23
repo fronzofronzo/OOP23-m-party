b63134e58707d6df35fa3b25751a8c0dfe125bb8
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-06-23 15:09:01 +0200

    update on shop interface

--- src/main/java/it/unibo/mparty/model/shop/api/Shop.java
+++ src/main/java/it/unibo/mparty/model/shop/api/Shop.java
@@ -1,5 +1,6 @@
 package it.unibo.mparty.model.shop.api;
 
+import it.unibo.mparty.model.item.api.Item;
 import it.unibo.mparty.model.item.impl.ItemName;
 import it.unibo.mparty.model.player.api.Player;
 
@@ -13,4 +14,11 @@ public interface Shop {
      * @param item the name of the item 
      */
     void buyItem(Player player, ItemName item);
+
+    /**
+     * get the item from the name
+     * @param item the name of the item
+     * @return the item considerated
+     */
+    Item getItem(ItemName item);
 }
