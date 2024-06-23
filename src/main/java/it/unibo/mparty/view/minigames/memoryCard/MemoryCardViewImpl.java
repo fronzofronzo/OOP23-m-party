5ebc871ba49afd734fe00edfe9819b497f07b4eb
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-23 15:55:13 +0200

    initialized MemoryCardViewImpl

--- src/main/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImpl.java
+++ src/main/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImpl.java
@@ -1,4 +1,24 @@
 package it.unibo.mparty.view.minigames.memoryCard;
 
-public class MemoryCardViewImpl {
+import it.unibo.mparty.controller.minigames.memoryCard.MemoryCardController;
+import it.unibo.mparty.controller.minigames.memoryCard.MemoryCardControllerImpl;
+
+public class MemoryCardViewImpl implements MemoryCardView{
+
+    private final MemoryCardController controller = new MemoryCardControllerImpl(this);
+
+    @Override
+    public void disableButton(int index) {
+
+    }
+
+    @Override
+    public void setTextButton(int index, String text) {
+
+    }
+
+    @Override
+    public void showResult() {
+
+    }
 }
