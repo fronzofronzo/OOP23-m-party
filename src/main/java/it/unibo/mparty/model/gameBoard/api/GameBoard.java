3f0f50cecdfefe206c9c772c74be184b7a144c00
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-06-24 17:18:13 +0200

    Wrote GameBoard javadoc

--- src/main/java/it/unibo/mparty/model/gameBoard/api/GameBoard.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/api/GameBoard.java
@@ -8,18 +8,47 @@ import it.unibo.mparty.model.gameBoard.util.Pair;
 import it.unibo.mparty.model.gameBoard.util.Position;
 import it.unibo.mparty.model.gameBoard.util.SlotType;
 
+/**
+ * This interface models a Game Board
+ */
+
 public interface GameBoard {
 
+    /**
+     * Get a Position in the Board Game then return its Slot Type
+     * @param position 
+     * @return the {@link SlotType}
+     */
     SlotType getSlotType(Position position);
     
+    /**
+     * Get the game board's starting position 
+     * @return the starting position
+     */
     Position getStrartingPosition();
     
+    /**
+     * Get the next(s) position(s) of the input position
+     * @param position a position 
+     * @return a map that contains directions and positions
+     */
     Map<Direction, Position> getNextPositions(Position position);
     
+    /**
+     * Get the actual position of the active star
+     * @return the actual position of the active star
+     */
     Position getStarPosition();
 
+    /**
+     * Change tha actual position of the active star
+     */
     void changeStarPosition();
 
+    /**
+     * Get the board's dimension
+     * @return width x height
+     */
     Pair<Integer, Integer> getDimension();
 
     void printBoard();
