f80671c3b03170aa3f8c9930651ec152effa810b
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-24 17:12:28 +0200

    added setScene method to View interface and implemented javadoc

--- src/main/java/it/unibo/mparty/view/View.java
+++ src/main/java/it/unibo/mparty/view/View.java
@@ -1,4 +1,13 @@
 package it.unibo.mparty.view;
 
+/**
+ * Models the view of the application
+ */
 public interface View {
+
+    /**
+     * Sets the next scene to show
+     * @param path of the scene to show
+     */
+    void setScene(String path);
 }
