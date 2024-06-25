94f73c5f96e9d2975e6385afd263a5e1dfc7f2e2
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-25 17:33:39 +0200

    removed useless libraries

--- src/test/java/it/unibo/samplejavafx/model/minigames/perilouspath/TestPerilousPath.java
+++ src/test/java/it/unibo/samplejavafx/model/minigames/perilouspath/TestPerilousPath.java
@@ -1,21 +1,14 @@
 package it.unibo.samplejavafx.model.minigames.perilouspath;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.Random;
 
-import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
 import it.unibo.mparty.model.minigames.perilouspath.api.PerilousPath;
-import it.unibo.mparty.model.minigames.perilouspath.api.PerilousPath.Type;
-import it.unibo.mparty.model.minigames.perilouspath.impl.BombPosition;
 import it.unibo.mparty.model.minigames.perilouspath.impl.PathPosition;
-import it.unibo.mparty.model.minigames.perilouspath.impl.Pair;
-import it.unibo.mparty.model.minigames.perilouspath.impl.BallPosition;
 import it.unibo.mparty.model.minigames.perilouspath.impl.PerilousPathImpl;
 
 
@@ -25,10 +18,9 @@ public class TestPerilousPath{
     private static int NUM_BALLS = 2;
     private static int NUM_BOMBS = 8;
     private static int FIRST_COLUMN = 0;
-    private static int LAST_COLUMN = 8;
+    private static int LAST_COLUMN = 7;
 
     private PerilousPath model = new PerilousPathImpl(SIDE);
-    private Random random = new Random();
 
     @Test
     public void TestPerilousPathConstructor(){
@@ -55,11 +47,31 @@ public class TestPerilousPath{
 
     @Test
     public void TestHit(){
-        var bomb = this.model.getBombs().get(FIRST_COLUMN);
-        assertEquals(Type.WRONG, this.model.hit(bomb));
+        this.model.setBalls();
+        this.model.setBombs();
+        var p = getSafePosition();
+        assertEquals(PerilousPath.Type.PATH, this.model.hit(p));
     }
 
-    /*private AbstractPosition getSafePosition(){
+    @Test
+    public void TestIsOver(){
+
         
-    }*/
+    }
+
+    private AbstractPosition getSafePosition(){
+        var p = new PathPosition(this.model.getBalls().get(0).getX() + 1,this.model.getBalls().get(0).getY(), SIDE);
+        if(!this.model.getBombs().contains(p)){
+            return p;
+        }
+        var p1 = new PathPosition(this.model.getBalls().get(0).getX(),this.model.getBalls().get(0).getY() + 1, SIDE);
+        if(!this.model.getBombs().contains(p1)){
+            return p1;
+        }
+        var p2 = new PathPosition(this.model.getBalls().get(0).getX() - 1,this.model.getBalls().get(0).getY(), SIDE);
+        return p2;
+        
+    }
+
+    
 }
