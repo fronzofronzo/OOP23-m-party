cb3dc7ba3af1005790a996a915ab95b18457efef
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-25 11:50:01 +0200

    implemented private method to generate a random minigame

--- src/main/java/it/unibo/mparty/model/minigameHandler/MinigameHandlerImplementation.java
+++ src/main/java/it/unibo/mparty/model/minigameHandler/MinigameHandlerImplementation.java
@@ -1,15 +1,20 @@
 package it.unibo.mparty.model.minigameHandler;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MinigameHandlerImplementation implements MinigameHandler{
 
     private String actualMinigame = null;
 
     @Override
-    public void startMinigame() {
-        final BufferedReader reader = new BufferedReader(new FileReader("singlePlayerMinigames.txt"))
+    public void startMinigame(){
+
     }
 
     @Override
@@ -22,7 +27,14 @@ public class MinigameHandlerImplementation implements MinigameHandler{
 
     }
 
-    private String getRandomMinigame(){
-
+    private String generateRandomMinigame() throws Exception {
+        final BufferedReader reader = new BufferedReader(new FileReader("singlePlayerMinigames.txt"));
+        String name = null;
+        final List<String> minigames = new ArrayList<>();
+        while ((name = reader.readLine()) != null){
+            minigames.add(name);
+        }
+        final Random random = new Random();
+        return minigames.get(random.nextInt(minigames.size()));
     }
 }
