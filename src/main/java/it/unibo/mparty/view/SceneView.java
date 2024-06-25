e2c08965bbeb55f45250eec835d18af89750d5d1
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-25 09:43:49 +0200

    Initialized SceneView interface

---
+++ src/main/java/it/unibo/mparty/view/SceneView.java
@@ -0,0 +1,13 @@
+package it.unibo.mparty.view;
+
+/**
+ * Interface that models a general scene of the application
+ */
+public interface SceneView {
+
+    /**
+     * gets the main view of reference
+     * @return the gameView
+     */
+    GameView getMainView();
+}
