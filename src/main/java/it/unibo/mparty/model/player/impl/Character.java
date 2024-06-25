3f8cb1e96b337da3b0126cdda1121aa1d8838d53
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-25 15:59:08 +0200

    modified Character enum and implemente name field

--- src/main/java/it/unibo/mparty/model/player/impl/Character.java
+++ src/main/java/it/unibo/mparty/model/player/impl/Character.java
@@ -1,5 +1,20 @@
 package it.unibo.mparty.model.player.impl;
 
 public enum Character {
-    CHAR_MARIO, CHAR_LUIGI, CHAR_PEACH, CHAR_WARIO, CHAR_YOSHI, CHAR_DK
+    CHAR_MARIO("Mario"),
+    CHAR_LUIGI("Luigi"),
+    CHAR_PEACH("Peach"),
+    CHAR_WARIO("Wario"),
+    CHAR_YOSHI("Yoshi"),
+    CHAR_DK("Donkey-Kong");
+
+    private final String name;
+
+    private Character(final String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
