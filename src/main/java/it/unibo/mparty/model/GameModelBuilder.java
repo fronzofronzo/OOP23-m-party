1971283380eede3c983ac0efca85ef695c7544db
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-25 15:18:11 +0200

    added javadoc of GameModelBuilder interface

--- src/main/java/it/unibo/mparty/model/GameModelBuilder.java
+++ src/main/java/it/unibo/mparty/model/GameModelBuilder.java
@@ -1,5 +1,9 @@
 package it.unibo.mparty.model;
 
+/**
+ * Interface of the GameModelBuilder: it's an application of the pattern
+ * Builder to build an instance of GameModel
+ */
 public interface GameModelBuilder {
 
 
