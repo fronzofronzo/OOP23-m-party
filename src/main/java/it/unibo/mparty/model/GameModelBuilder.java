1bc06900d04913438abedb06c6c61bae13a671a0
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-25 15:16:19 +0200

    initialised GameModelBuilder interface and class

---
+++ src/main/java/it/unibo/mparty/model/GameModelBuilder.java
@@ -0,0 +1,6 @@
+package it.unibo.mparty.model;
+
+public interface GameModelBuilder {
+
+
+}
---
+++ src/main/java/it/unibo/mparty/model/GameModelBuilderImpl.java
@@ -0,0 +1,4 @@
+package it.unibo.mparty.model;
+
+public class GameModelBuilderImpl implements GameModelBuilder{
+}
