46361abefaa1da55d368e3a84d7dd2729179d3f4
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-25 17:32:42 +0200

    modified tests of constructor and stars amount implementation

--- src/test/java/it/unibo/samplejavafx/model/Player/TestPlayerImplementation.java
+++ src/test/java/it/unibo/samplejavafx/model/Player/TestPlayerImplementation.java
@@ -7,6 +7,7 @@ import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.impl.Character;
 import it.unibo.mparty.model.player.impl.PlayerImplementation;
 import it.unibo.mparty.utilities.Position;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestPlayerImplementation {
@@ -14,24 +15,25 @@ public class TestPlayerImplementation {
     @Test
     public void testPlayerConstructor(){
         final String username = "username";
-        final Character character = Character.CHAR_LUIGI;
+        final String character = "Luigi";
         final Position position = new Position(0,0);
-        final Player testPlayer = new PlayerImplementation(username,position,character);
+        final Player testPlayer = new PlayerImplementation(username,character);
         assertEquals(username, testPlayer.getUsername());
-        assertEquals(character, testPlayer.getCharacter());
+        assertEquals(character, testPlayer.getCharacter().getName());
         assertEquals(position, testPlayer.getPosition());
     }
 
     @Test
     public void testStarAmount(){
         final String username = "username";
-        final Character character = Character.CHAR_LUIGI;
+        final String character = "Luigi";
         final Position position = new Position(0,0);
-        final Player testPlayer = new PlayerImplementation(username,position,character);
+        final Player testPlayer = new PlayerImplementation(username,character);
         assertEquals(0, testPlayer.getNumStars());
         testPlayer.addStar();
         assertEquals(1, testPlayer.getNumStars());
-        testPlayer.
+        testPlayer.removeStar();
+        assertEquals(0, testPlayer.getNumStars());
     }
 
 
