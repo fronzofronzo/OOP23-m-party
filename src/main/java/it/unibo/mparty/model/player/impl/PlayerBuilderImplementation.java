2f36225526b3093a03a57db21f9c1f6e82a65829
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-26 14:34:12 +0200

    modified Position import in PlayerBuilder

--- src/main/java/it/unibo/mparty/model/player/impl/PlayerBuilderImplementation.java
+++ src/main/java/it/unibo/mparty/model/player/impl/PlayerBuilderImplementation.java
@@ -2,6 +2,7 @@ package it.unibo.mparty.model.player.impl;
 
 import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.api.PlayerBuilder;
+import it.unibo.mparty.utilities.Position;
 
 public class PlayerBuilderImplementation implements PlayerBuilder {
 
