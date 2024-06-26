6f5c5599a160ad82f72f929fa6ab3df0f6ea7d9a
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-26 14:37:46 +0200

    modified PlayerBuilderImpl

--- src/main/java/it/unibo/mparty/model/player/impl/PlayerBuilderImplementation.java
+++ src/main/java/it/unibo/mparty/model/player/impl/PlayerBuilderImplementation.java
@@ -7,7 +7,7 @@ import it.unibo.mparty.utilities.Position;
 public class PlayerBuilderImplementation implements PlayerBuilder {
 
     private String username;
-    private Character character;
+    private String character;
     private Position position;
 
     @Override
@@ -17,7 +17,7 @@ public class PlayerBuilderImplementation implements PlayerBuilder {
     }
 
     @Override
-    public PlayerBuilder character(Character character) {
+    public PlayerBuilder character(String character) {
         this.character = character;
         return this;
     }
@@ -33,6 +33,6 @@ public class PlayerBuilderImplementation implements PlayerBuilder {
         if (this.username == null || this.position == null || this.character == null){
             throw new IllegalStateException("Missing fields: cannot create a new Player");
         }
-        return new PlayerImplementation(this.username, this.position, this.character);
+        return new PlayerImplementation(this.username, this.character);
     }
 }
