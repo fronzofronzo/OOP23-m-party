41f3716558da40f9b757ad870e41b1cdd33d9675
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-26 19:48:13 +0200

    fixed BoardFactory

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardFactoryImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardFactoryImpl.java
@@ -3,52 +3,61 @@ package it.unibo.mparty.model.minigames.nanogram.impl;
 import it.unibo.mparty.model.minigames.nanogram.api.Board;
 import it.unibo.mparty.model.minigames.nanogram.api.BoardFactory;
 import it.unibo.mparty.model.minigames.nanogram.util.CellState;
-import it.unibo.mparty.utilities.Pair;
+import it.unibo.mparty.utilities.Position;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class BoardFactoryImpl implements BoardFactory {
 
     @Override
-    public Board createSimpleBoard(final int size, final int fillPercentage) {
-        final Map<Pair<Integer, Integer>, CellState> grid = new HashMap<>();
-        final Map<Pair<Integer, Integer>, CellState> showGrid = new HashMap<>();
+    public Board createSimpleBoard(int size, int fillPercentage) {
+        Map<Position, CellState> grid = new HashMap<>();
+        Map<Position, CellState> showGrid = new HashMap<>();
 
+        List<Position> positions = generateAllPositions(size);
         int totalCells = size * size;
         int filledCellsCount = (int) Math.round(totalCells * (fillPercentage / 100.0));
 
-        List<Pair<Integer, Integer>> positions = IntStream.range(0, size)
-                .boxed()
-                .flatMap(i -> IntStream.range(0, size).mapToObj(j -> new Pair<>(i, j)))
-                .collect(Collectors.toList());
+        positions.subList(0, filledCellsCount).forEach(pos -> grid.put(pos, CellState.FILLED));
+        positions.subList(filledCellsCount, totalCells).forEach(pos -> grid.put(pos, CellState.MARKED));
 
-        Collections.shuffle(positions);
-        positions.stream().limit(filledCellsCount).forEach(pos -> grid.put(pos, CellState.FILLED));
-        positions.stream().skip(filledCellsCount).forEach(pos -> grid.put(pos, CellState.MARKED));
+        grid.forEach((pos, state) -> showGrid.put(pos, CellState.MARKED));
 
-        positions.forEach(pos -> showGrid.put(pos, CellState.MARKED));
-
-        return new BoardImpl(grid, showGrid);
+        return new BoardImpl(grid, showGrid, size);
     }
 
     @Override
-    public Board createHardBoard(final int size, final int fillPercentage, final int showPercentage) {
-        final Board grid = createSimpleBoard(size, fillPercentage);
-        final Map<Pair<Integer, Integer>, CellState> showGrid = new HashMap<>(grid.getShowGrid());
+    public Board createHardBoard(int size, int fillPercentage, int showPercentage) {
+        Board simpleBoard = createSimpleBoard(size, fillPercentage);
+        Map<Position, CellState> grid = new HashMap<>(simpleBoard.getGrid());
+        Map<Position, CellState> showGrid = new HashMap<>(simpleBoard.getShowGrid());
 
-        int filledCellsCount = (int) grid.getGrid().values().stream().filter(state -> state == CellState.FILLED).count();
+        int filledCellsCount = countFilledCells(grid);
         int showCellsCount = (int) Math.round(filledCellsCount * (showPercentage / 100.0));
 
-        List<Pair<Integer, Integer>> filledPositions = grid.getGrid().entrySet().stream()
+        List<Position> filledPositions = grid.entrySet().stream()
                 .filter(entry -> entry.getValue() == CellState.FILLED)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
 
         Collections.shuffle(filledPositions);
-        filledPositions.stream().limit(showCellsCount).forEach(pos -> showGrid.put(pos, CellState.FILLED));
+        filledPositions.subList(0, showCellsCount).forEach(pos -> showGrid.put(pos, CellState.FILLED));
 
-        return new BoardImpl(grid.getGrid(), showGrid);
+        return new BoardImpl(grid, showGrid, size);
+    }
+
+    private List<Position> generateAllPositions(int size) {
+        List<Position> positions = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                positions.add(new Position(i, j));
+            }
+        }
+        return positions;
+    }
+
+    private int countFilledCells(Map<Position, CellState> grid) {
+        return (int) grid.values().stream().filter(state -> state == CellState.FILLED).count();
     }
 }
