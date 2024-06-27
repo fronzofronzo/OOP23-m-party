e9e09093497038413ca3781cf6780eee6e612f50
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-06-27 14:36:46 +0200

    changed the method hit() in MemorySweepImpl

--- src/main/java/it/unibo/mparty/model/minigames/memorysweep/impl/MemorySweepImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/memorysweep/impl/MemorySweepImpl.java
@@ -13,7 +13,7 @@ import it.unibo.mparty.utilities.Position;
 public class MemorySweepImpl implements MemorySweep{
 
     private Set<Position> randomList;
-    private int counter = 3;
+    private int counter;
     private Set<Position> player1;
     private Set<Position> player2;
     private Random random;
@@ -26,6 +26,7 @@ public class MemorySweepImpl implements MemorySweep{
         this.player1 = new HashSet<>();
         this.player2 = new HashSet<>();
         this.side = side;
+        this.counter = 3;
     }
 
     /*
@@ -33,7 +34,7 @@ public class MemorySweepImpl implements MemorySweep{
      */
     @Override
     public Set<Position> getRandomList() {
-        this.counter++;
+        this.setCounter();
         this.randomList.clear();
         for(var i = 0; i < this.counter; i++){
             this.randomList.add(new Position(random.nextInt(this.side),random.nextInt(this.side)));
@@ -41,26 +42,49 @@ public class MemorySweepImpl implements MemorySweep{
         return this.randomList;
     }
 
+    /*
+     * DA SISTEMARE
+     */
     @Override
     public HitType hit(Position p) {
-        if(turn){
-            if(this.player1.size() < this.randomList.size()){
+        if(this.getTurn()){
+            if(this.randomList.contains(p)){
                 this.player1.add(p);
-                return HitType.FIRST;
+                if(this.player1.size() == this.randomList.size()){
+                    this.turn = false;
+                    this.player1.clear();
+                    return HitType.TURN_END;//player 1 ha passato il turno tocca al player 2
+                }
+                return HitType.RIGHT_CHOICE;
             }
-            this.turn = false;
+            return HitType.LOSS;//player 1 ha perso,ha vinto player 2
         }
-        if(this.player2.size() < this.randomList.size()){
+        
+        if(this.randomList.contains(p)){
             this.player2.add(p);
+            if(this.player2.size() == this.randomList.size()){
+                this.turn = true;
+                this.player2.clear();
+                return HitType.TURN_END;//player 2 ha passato il turno tocca al player 1
+            }
+            return HitType.RIGHT_CHOICE;
         }
-        this.turn = true;
-        return HitType.SECOND;
+        return HitType.LOSS;//player 2 ha perso,ha vinto player 1
     }
 
     @Override
     public boolean getTurn() {
+        return this.turn;
+    }
+
+    @Override
+    public boolean isOver(Set<Position> player) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTurn'");
+        throw new UnsupportedOperationException("Unimplemented method 'isOver'");
+    }
+
+    private void setCounter(){
+        this.counter++;
     }
 
     
