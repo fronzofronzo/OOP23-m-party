8a421aba49602a59da1485c1f5721e241a406c1e
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-27 15:26:54 +0200

    implemented getMainController method

--- src/main/java/it/unibo/mparty/view/AbstractSceneView.java
+++ src/main/java/it/unibo/mparty/view/AbstractSceneView.java
@@ -18,5 +18,10 @@ public abstract class AbstractSceneView implements SceneView{
         this.mainController = controller;
     }
 
+    @Override
+    public GameController getMainController(){
+        return this.mainController;
+    }
+
 
 }
