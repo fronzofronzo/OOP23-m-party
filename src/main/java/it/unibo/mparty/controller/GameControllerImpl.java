1e2ba413268accdf8812fff78bac4a7c52a8ae33
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-28 15:13:20 +0200

    added private field of model and view and intialized view in constructor

--- src/main/java/it/unibo/mparty/controller/GameControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/GameControllerImpl.java
@@ -1,8 +1,16 @@
 package it.unibo.mparty.controller;
 
+import it.unibo.mparty.model.GameModel;
+import it.unibo.mparty.view.GameView;
+
 public class GameControllerImpl implements GameController{
 
+    private final GameView view;
+    private GameModel model;
 
+    public GameControllerImpl(final GameView view){
+        this.view = view;
+    }
 
     @Override
     public void rollDice() {
