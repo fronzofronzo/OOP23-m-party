4b8bea60df2641d15245d05bc094408b30a6e149
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-28 15:08:28 +0200

    added method to Game Controller interface

--- src/main/java/it/unibo/mparty/controller/GameController.java
+++ src/main/java/it/unibo/mparty/controller/GameController.java
@@ -1,4 +1,14 @@
 package it.unibo.mparty.controller;
 
 public interface GameController {
+
+    void rollDice();
+
+    void movePlayer();
+
+    boolean buyItem();
+
+    void startGame();
+
+    void endGame();
 }
