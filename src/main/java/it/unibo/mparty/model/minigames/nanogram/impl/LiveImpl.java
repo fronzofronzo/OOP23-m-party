5aa2aac75583470e1a5df6c0bb75010899675cb7
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-28 15:37:10 +0200

    added javadoc for LiveImpl

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/LiveImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/LiveImpl.java
@@ -2,9 +2,21 @@ package it.unibo.mparty.model.minigames.nanogram.impl;
 
 import it.unibo.mparty.model.minigames.nanogram.api.Live;
 
+/**
+ * Implementation of the Live interface for managing lives in a game.
+ * This class tracks the number of lives a player has and provides methods to
+ * retrieve, reset, update, and check the status of lives.
+ */
 public class LiveImpl implements Live {
     private static final int INITIAL_LIVES = 3;
     private int lives;
+
+    /**
+     * Constructs a LiveImpl instance with the initial number of lives set to {@value #INITIAL_LIVES}.
+     */
+    public LiveImpl() {
+        this.lives = INITIAL_LIVES;
+    }
 
     @Override
     public int getLive() {
