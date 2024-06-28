a1b806a8dfc3f9dfe100fa5488f9f6ccbd180844
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-06-28 16:19:34 +0200

    Modified GameBoardView

--- src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardViewImpl.java
+++ src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardViewImpl.java
@@ -1,5 +1,12 @@
 package it.unibo.mparty.view.GameBoardView;
 
-public class GameBoardViewImpl {
+import it.unibo.mparty.model.gameBoard.util.SlotType;
+import it.unibo.mparty.view.AbstractSceneView;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 
+public class GameBoardViewImpl extends AbstractSceneView implements GameBoardView{
+
+    @FXML
+    private GridPane board;
 }
