e40b6333b9148c12c10a55c75442e45e93e1c059
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-06-28 15:10:07 +0200

    added methods of interface to Game controller implementation

--- src/main/java/it/unibo/mparty/controller/GameControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/GameControllerImpl.java
@@ -1,4 +1,31 @@
 package it.unibo.mparty.controller;
 
 public class GameControllerImpl implements GameController{
+
+
+
+    @Override
+    public void rollDice() {
+
+    }
+
+    @Override
+    public void movePlayer() {
+
+    }
+
+    @Override
+    public boolean buyItem() {
+        return false;
+    }
+
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void endGame() {
+
+    }
 }
