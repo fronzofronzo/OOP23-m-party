a11becaa3e1e063fe2b7e1980b7580ea0a3147bb
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-06-28 14:36:22 +0200

    added some changes to the shop, eliminated shop.fxml

--- src/main/java/it/unibo/mparty/view/shop/ShopView.java
+++ src/main/java/it/unibo/mparty/view/shop/ShopView.java
@@ -3,5 +3,5 @@ package it.unibo.mparty.view.shop;
 import it.unibo.mparty.view.SceneView;
 
 public interface ShopView extends SceneView{
-
+    
 }
--- src/main/java/it/unibo/mparty/view/shop/ShopViewImpl.java
+++ src/main/java/it/unibo/mparty/view/shop/ShopViewImpl.java
@@ -1,8 +1,7 @@
 package it.unibo.mparty.view.shop;
 
-import it.unibo.mparty.controller.GameController;
 import it.unibo.mparty.view.AbstractSceneView;
-import it.unibo.mparty.view.GameView;
+
 
 public class ShopViewImpl extends AbstractSceneView implements ShopView{
 
