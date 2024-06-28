ebb7c2e4e49c2077d854586a6e231fe4bc989fd2
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-06-28 16:27:14 +0200

    worked on the model for connect 4

---
+++ src/main/java/it/unibo/mparty/model/minigames/connect4/api/Connect4Model.java
@@ -0,0 +1,11 @@
+package it.unibo.mparty.model.minigames.connect4.api;
+
+
+
+public interface Connect4Model {
+    public int getResults();
+
+    public boolean isOver();
+
+    public boolean hasWon();
+}
---
+++ src/main/java/it/unibo/mparty/model/minigames/connect4/impl/Connect4ModelImpl.java
@@ -0,0 +1,25 @@
+package it.unibo.mparty.model.minigames.connect4.impl;
+
+import it.unibo.mparty.model.minigames.connect4.api.Connect4Model;
+
+public class Connect4ModelImpl implements Connect4Model{
+
+    @Override
+    public int getResults() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getResults'");
+    }
+
+    @Override
+    public boolean isOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isOver'");
+    }
+
+    @Override
+    public boolean hasWon() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasWon'");
+    }
+
+}
