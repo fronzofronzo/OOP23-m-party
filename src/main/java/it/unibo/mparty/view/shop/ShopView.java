723e2c3f11360748ab828a2a567a8631f668ab20
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-06-28 15:34:57 +0200

    modified some view methods

--- src/main/java/it/unibo/mparty/view/shop/ShopView.java
+++ src/main/java/it/unibo/mparty/view/shop/ShopView.java
@@ -2,6 +2,10 @@ package it.unibo.mparty.view.shop;
 
 import it.unibo.mparty.view.SceneView;
 
+/**
+ * non so cosa scrivere
+ */
 public interface ShopView extends SceneView{
     
+    public void setUpShop();
 }
--- src/main/java/it/unibo/mparty/view/shop/ShopViewImpl.java
+++ src/main/java/it/unibo/mparty/view/shop/ShopViewImpl.java
@@ -1,8 +1,23 @@
 package it.unibo.mparty.view.shop;
 
 import it.unibo.mparty.view.AbstractSceneView;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 
 public class ShopViewImpl extends AbstractSceneView implements ShopView{
 
+    @FXML
+    private AnchorPane descriptionPane;
+
+    @FXML
+    private AnchorPane buttonPane;
+
+    @Override
+    public void setUpShop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setUpShop'");
+    }
+
+    
 }
