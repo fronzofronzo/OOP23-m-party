d1cf4b1b503b642ae6f99fe4d56dd32238d330a4
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-29 15:29:23 +0200

    implemented DominoFactory

---
+++ src/main/java/it/unibo/mparty/model/minigames/domino/impl/DominoFactoryImpl.java
@@ -0,0 +1,23 @@
+package it.unibo.mparty.model.minigames.domino.impl;
+
+import it.unibo.mparty.model.minigames.domino.api.Domino;
+import it.unibo.mparty.model.minigames.domino.api.DominoFactory;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class DominoFactoryImpl implements DominoFactory {
+
+    @Override
+    public Domino createDomino(final int sideA, final int sideB) {
+        return new DominoImpl(sideA, sideB);
+    }
+
+    @Override
+    public Set<Domino> createFullSet() {
+        return IntStream.rangeClosed(0, 6).boxed()
+                .flatMap(i -> IntStream.rangeClosed(i, 6).mapToObj(j -> new DominoImpl(i, j)))
+                .collect(Collectors.toSet());
+    }
+}
