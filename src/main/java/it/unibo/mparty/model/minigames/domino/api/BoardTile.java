a4af6245c2ab8627c5b854e8a76b258f385683e9
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-29 22:58:30 +0200

    implemented placeTile method of BoardTile

---
+++ src/main/java/it/unibo/mparty/model/minigames/domino/api/BoardTile.java
@@ -0,0 +1,5 @@
+package it.unibo.mparty.model.minigames.domino.api;
+
+public interface BoardTile {
+    boolean canPlaceTile(Tile tile);
+}
---
+++ src/main/java/it/unibo/mparty/model/minigames/domino/impl/BoardTileImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.mparty.model.minigames.domino.impl;
+
+import it.unibo.mparty.model.minigames.domino.api.BoardTile;
+import it.unibo.mparty.model.minigames.domino.api.Tile;
+
+import java.util.LinkedList;
+
+public class BoardTileImpl implements BoardTile {
+    private LinkedList<Tile> boardTiles;
+    private int leftExtrem;
+    private int rightExtrem;
+
+    public BoardTileImpl() {
+        this.boardTiles = new LinkedList<>();
+    }
+
+    @Override
+    public boolean canPlaceTile(Tile tile){
+        if (this.boardTiles.isEmpty()){
+            this.boardTiles.add(tile);
+            this.leftExtrem = this.boardTiles.getFirst().getSideA();
+            this.rightExtrem = this.boardTiles.getFirst().getSideB();
+            return true;
+        }
+
+        if (tile.canMatchLeft(leftExtrem)){
+            leftExtrem = tile.getSideA();
+            this.boardTiles.addFirst(tile);
+            return true;
+        } else if (tile.canMatchRight(rightExtrem)){
+            rightExtrem = tile.getSideB();
+            this.boardTiles.addLast(tile);
+            return true;
+        }
+
+        return false;
+    }
+}
