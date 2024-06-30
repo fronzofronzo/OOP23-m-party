a1a7f52be91baec4769a0b6d384b991a1ffbb099
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-30 22:37:46 +0200

    implemented tiles view

--- src/main/java/it/unibo/mparty/view/minigames/domino/impl/DominoViewImpl.java
+++ src/main/java/it/unibo/mparty/view/minigames/domino/impl/DominoViewImpl.java
@@ -3,6 +3,8 @@ package it.unibo.mparty.view.minigames.domino.impl;
 import it.unibo.mparty.controller.minigames.domino.api.DominoController;
 import it.unibo.mparty.controller.minigames.domino.impl.DominoControllerImpl;
 import it.unibo.mparty.model.minigames.domino.api.Tile;
+import it.unibo.mparty.model.player.api.Player;
+import it.unibo.mparty.model.player.impl.PlayerImplementation;
 import it.unibo.mparty.view.AbstractSceneView;
 import it.unibo.mparty.view.minigames.domino.api.DominoView;
 
@@ -10,11 +12,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import java.util.Set;
 
 public class DominoViewImpl extends AbstractSceneView implements DominoView {
+
+    private static final int PREF_SIZE = 50;
 
     @FXML
     private Button drawButton;
@@ -37,10 +42,18 @@ public class DominoViewImpl extends AbstractSceneView implements DominoView {
     @FXML
     private Button putButton;
 
-    private final DominoController controller;
+    private DominoController controller;
 
-    public DominoViewImpl() {
-        this.controller = new DominoControllerImpl(this);
+    @FXML
+    private void initialize() {
+        //todo: da cancellare
+        this.initPlayers(new PlayerImplementation("Player1", "Luigi"), new PlayerImplementation("Player2", "Mario"));
+    }
+
+    //todo: da mettere nell'interfaccia e deve essere chiamato da fuori (gioco principale)
+    public void initPlayers(Player player1, Player player2) {
+        this.controller = new DominoControllerImpl(this, player1, player2);
+        this.controller.setUp();
     }
 
     @FXML
@@ -54,22 +67,36 @@ public class DominoViewImpl extends AbstractSceneView implements DominoView {
     }
 
     @Override
-    public void setPlayerTiles(boolean isPlayer1, Set<Tile> playerTiles){
-        //todo aggiungi p1 e p2 i loro tiles
-        if (isPlayer1){
+    public void setPlayerTiles(boolean isPlayer1, Set<Tile> playerTiles) {
+        if (isPlayer1) {
             this.player1Tiles.getChildren().clear();
-            for (Tile tile : playerTiles){
-                this.player1Tiles.getChildren().add(this.createTileView(tile));
+            for (Tile tile : playerTiles) {
+                this.player1Tiles.getChildren().add(generateDomino(tile.getSideA().getValue(), tile.getSideB().getValue()));
             }
-        } else{
+        } else {
             this.player2Tiles.getChildren().clear();
-            for (Tile tile : playerTiles){
-                this.player2Tiles.getChildren().add(this.createTileView(tile));
+            for (Tile tile : playerTiles) {
+                this.player2Tiles.getChildren().add(generateDomino(tile.getSideA().getValue(), tile.getSideB().getValue()));
             }
         }
     }
 
-    private Text createTileView(Tile tile) {
-        return new Text("[" + tile.getSideA() + "|" + tile.getSideB() + "]");
+    private VBox generateDomino(int valueA, int valueB) {
+        VBox box = new VBox();
+        box.setPrefSize(PREF_SIZE * 2, PREF_SIZE);
+        VBox.setMargin(box, new Insets(0, PREF_SIZE, 0, PREF_SIZE));
+        Button sideA = generateSide(valueA);
+        Button sideB = generateSide(valueB);
+
+        box.getChildren().add(sideA);
+        box.getChildren().add(sideB);
+        return box;
+    }
+
+    private Button generateSide(int value) {
+        Button button = new Button(String.valueOf(value));
+        button.setPrefSize(PREF_SIZE, PREF_SIZE);
+        //button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        return button;
     }
 }
