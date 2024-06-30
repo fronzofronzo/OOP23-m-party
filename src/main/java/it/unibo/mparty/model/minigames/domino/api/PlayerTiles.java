f08c18a45f7c4c5c7b2af754da77f108c4641faf
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-30 22:35:13 +0200

    refactor PlayerTiles

--- src/main/java/it/unibo/mparty/model/minigames/domino/api/PlayerTiles.java
+++ src/main/java/it/unibo/mparty/model/minigames/domino/api/PlayerTiles.java
@@ -7,6 +7,8 @@ import java.util.Set;
 public interface PlayerTiles {
     void addTilesToPlayer(Player player, Set<Tile> tiles);
 
+    void addTileToPlayer(Player player, Tile tile);
+
     void removeTilesFromPlayer(Player player, Tile tile);
 
     Set<Tile> getPlayerTiles(Player player);
--- src/main/java/it/unibo/mparty/model/minigames/domino/impl/PlayerTilesImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/domino/impl/PlayerTilesImpl.java
@@ -11,31 +11,38 @@ import java.util.Set;
 
 public class PlayerTilesImpl implements PlayerTiles {
 
-    private final Map<Player,Set<Tile>> playerTiles = new HashMap<>();
+    private final Map<Player, Set<Tile>> playersTiles = new HashMap<>();
+
 
     @Override
     public void addTilesToPlayer(final Player player, final Set<Tile> tiles) {
-        if (!playerTiles.containsKey(player) || this.playerTiles.get(player).isEmpty()){
-            this.playerTiles.computeIfAbsent(player, k -> new HashSet<>()).addAll(tiles);
-        } else {
-            Tile tile = tiles.stream().findFirst().orElse(null);
-            if (tile != null) {
-                this.playerTiles.computeIfAbsent(player, k -> new HashSet<>()).add(tile);
-                tiles.remove(tile);
-            }
-        }
+        playersTiles.put(player, tiles);
+
+//        if (!playersTiles.containsKey(player) || this.playersTiles.get(player).isEmpty()) {
+//            this.playersTiles.computeIfAbsent(player, k -> new HashSet<>()).addAll(tiles);
+//        } else {
+//            Tile tile = tiles.stream().findFirst().orElse(null);
+//            if (tile != null) {
+//                Set<Tile> set = this.playersTiles.get(player);
+//                set.put(tile);
+//                playersTiles.replace(player, set);
+//
+//                this.playersTiles.computeIfAbsent(player, k -> new HashSet<>()).add(tile);
+//                tiles.remove(tile);
+//            }
+//        }
     }
 
     @Override
-    public void removeTilesFromPlayer(final Player player, final Tile tile){
-        Set<Tile> tiles = this.playerTiles.get(player);
-        if(tiles != null) {
+    public void removeTilesFromPlayer(final Player player, final Tile tile) {
+        Set<Tile> tiles = this.playersTiles.get(player);
+        if (tiles != null) {
             tiles.remove(tile);
         }
     }
 
     @Override
-    public Set<Tile> getPlayerTiles(final Player player){
-        return this.playerTiles.getOrDefault(player, new HashSet<>());
+    public Set<Tile> getPlayerTiles(final Player player) {
+        return this.playersTiles.getOrDefault(player, new HashSet<>());
     }
 }
