3a343de08a20d1d4aba911653719c66a0e40082e
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-30 17:59:30 +0200

    change scene path in domino

--- src/main/java/it/unibo/mparty/view/GameViewImpl.java
+++ src/main/java/it/unibo/mparty/view/GameViewImpl.java
@@ -32,6 +32,6 @@ public class GameViewImpl extends Application implements GameView{
     public void start(Stage primaryStage) throws Exception {
         this.stage = primaryStage;
         this.stage.show();
-        this.setScene("GameBoard.fxml");
+        this.setScene("minigames/domino.fxml");
     }
 }
