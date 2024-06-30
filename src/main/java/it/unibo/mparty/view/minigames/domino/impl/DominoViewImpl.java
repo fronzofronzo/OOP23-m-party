8683ff38785e8b96dd2fda8d3bb1a5dda840962f
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-30 17:01:15 +0200

    initialized domino view

--- src/main/java/it/unibo/mparty/view/minigames/domino/impl/DominoViewImpl.java
+++ src/main/java/it/unibo/mparty/view/minigames/domino/impl/DominoViewImpl.java
@@ -2,5 +2,42 @@ package it.unibo.mparty.view.minigames.domino.impl;
 
 import it.unibo.mparty.view.minigames.domino.api.DominoView;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+
+
 public class DominoViewImpl implements DominoView {
+    @FXML
+    private Button drawButton;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private Label player1Label;
+
+    @FXML
+    private HBox player1Tiles;
+
+    @FXML
+    private Label player2Label;
+
+    @FXML
+    private HBox player2Tiles;
+
+    @FXML
+    private Button putButton;
+
+    @FXML
+    private void drawButtonClicked() {
+
+    }
+
+    @FXML
+    private void playButtonClicked() {
+
+    }
 }
