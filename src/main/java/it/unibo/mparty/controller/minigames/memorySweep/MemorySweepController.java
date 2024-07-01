9fe6f19c238c7ac4fe2d0306783b6f824ad578a8
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-01 16:44:34 +0200

    let MemorySweepControllerImpl implement MemorySweepController

---
+++ src/main/java/it/unibo/mparty/controller/minigames/memorySweep/MemorySweepController.java
@@ -0,0 +1,4 @@
+package it.unibo.mparty.controller.minigames.memorySweep;
+
+public interface MemorySweepController {
+}
---
+++ src/main/java/it/unibo/mparty/controller/minigames/memorySweep/MemorySweepControllerImpl.java
@@ -0,0 +1,4 @@
+package it.unibo.mparty.controller.minigames.memorySweep;
+
+public class MemorySweepControllerImpl implements MemorySweepController {
+}
