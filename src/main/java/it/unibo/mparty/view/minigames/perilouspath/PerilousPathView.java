4b51572c0ce702d6373a69ce060baab8142fb126
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-01 09:36:32 +0200

    changed the methods for the view interface for Perilous Path

--- src/main/java/it/unibo/mparty/view/minigames/perilouspath/PerilousPathView.java
+++ src/main/java/it/unibo/mparty/view/minigames/perilouspath/PerilousPathView.java
@@ -1,16 +1,20 @@
 package it.unibo.mparty.view.minigames.perilouspath;
 
 import it.unibo.mparty.controller.minigames.perilousPath.PerilousPathController;
+import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
+import it.unibo.mparty.model.minigames.perilouspath.api.PerilousPath;
 import it.unibo.mparty.view.SceneView;
+
+import java.util.List;
 
 public interface PerilousPathView  extends SceneView{
 
     public void setObserver(PerilousPathController controller);
 
-    public void setUpView();
+    public void setUpView(List<AbstractPosition> balls,List<AbstractPosition> bombs);
 
-    public void hideBombs();
+    public void hideBombs(List<AbstractPosition> bombs);
 
-    public void hitTile();
+    public void hitTile(PerilousPath.Type type);
     
 }
