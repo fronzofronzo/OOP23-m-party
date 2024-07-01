fd705a325623cb896dd73393d319f7c213c41197
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-01 14:12:17 +0200

    modified the logic behind the waiting time before hiding the bombs in the PerilousPathControllerImpl class

--- src/main/java/it/unibo/mparty/controller/minigames/perilousPath/PerilousPathControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/perilousPath/PerilousPathControllerImpl.java
@@ -4,6 +4,8 @@ import it.unibo.mparty.model.minigames.perilouspath.api.PerilousPath;
 import it.unibo.mparty.model.minigames.perilouspath.impl.PerilousPathImpl;
 import it.unibo.mparty.view.minigames.perilouspath.PerilousPathView;
 import it.unibo.mparty.view.minigames.perilouspath.PerilousPathViewImpl;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 public class PerilousPathControllerImpl implements  PerilousPathController{
 
@@ -11,6 +13,7 @@ public class PerilousPathControllerImpl implements  PerilousPathController{
     private final PerilousPathView view;
     private final static int SIZE = 8;
     private final static int SECONDS = 6000;
+    private final PauseTransition pause = new PauseTransition(new Duration(SECONDS));
 
     public PerilousPathControllerImpl(){
         this.model = new PerilousPathImpl(SIZE);
@@ -24,8 +27,8 @@ public class PerilousPathControllerImpl implements  PerilousPathController{
     public void setUp() throws InterruptedException {
         this.view.setObserver(this);
         this.view.setUpView(this.model.getBalls(),this.model.getBombs());
-        Thread.sleep(SECONDS);
-        this.view.hideBombs(this.model.getBombs());
+        pause.setOnFinished(e -> this.view.hideBombs(this.model.getBombs()));
+        pause.play();
     }
 
     @Override
