e88150e8696a264fddf94720f827f59d1a0fde13
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-02 01:07:56 +0200

    deleted useless class

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/board/api/HardBoard.java
+++
@@ -1,4 +0,0 @@
-package it.unibo.mparty.model.minigames.nanogram.board.api;
-
-public interface HardBoard extends Board {
-}
--- src/main/java/it/unibo/mparty/model/minigames/nanogram/board/impl/HardBoardImpl.java
+++
@@ -1,6 +0,0 @@
-package it.unibo.mparty.model.minigames.nanogram.board.impl;
-
-import it.unibo.mparty.model.minigames.nanogram.board.api.HardBoard;
-
-public class HardBoardImpl {
-}
