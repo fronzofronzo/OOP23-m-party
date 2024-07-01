2b44ab0b39348086430d8db61a88817e2374222c
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-01 14:47:43 +0200

    Set stage and root size based on screen dimensions

--- src/main/java/it/unibo/mparty/view/GameViewImpl.java
+++ src/main/java/it/unibo/mparty/view/GameViewImpl.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import it.unibo.mparty.controller.GameController;
 import java.io.IOException;
@@ -21,7 +22,7 @@ public class GameViewImpl extends Application implements GameView{
     public void setScene(String path) throws IOException {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource(PATH + path)) ;
         final Parent root = loader.load(getClass().getResourceAsStream(PATH + path));
-        final Scene scene = new Scene(root, root.prefWidth(DEFAULT_DIMENSION_VALUE), root.prefHeight(DEFAULT_DIMENSION_VALUE));
+        final Scene scene = new Scene(root, this.stage.getWidth(), this.stage.getHeight());
         final SceneView sceneView = loader.<SceneView>getController();
         sceneView.init(this,this.controller);
         this.stage.setScene(scene);
@@ -31,7 +32,13 @@ public class GameViewImpl extends Application implements GameView{
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.stage = primaryStage;
+        this.setStageSize();
         this.stage.show();
         this.setScene("GameBoard.fxml");
+    }
+
+    private void setStageSize(){
+        this.stage.setHeight(Screen.getPrimary().getBounds().getHeight()/1.4);
+        this.stage.setWidth(Screen.getPrimary().getBounds().getWidth()/1.5);
     }
 }
