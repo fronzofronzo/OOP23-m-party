0b632e31bf9241b171dee6805a97e2291aa94f36
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-01 17:29:51 +0200

    Added unimplemented methods in GameBoardViewImpl

--- src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardViewImpl.java
+++ src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardViewImpl.java
@@ -1,9 +1,12 @@
 package it.unibo.mparty.view.GameBoardView;
 
+import java.util.Map;
+import java.util.Set;
+
 import it.unibo.mparty.model.gameBoard.util.SlotType;
+import it.unibo.mparty.utilities.Position;
 import it.unibo.mparty.view.AbstractSceneView;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class GameBoardViewImpl extends AbstractSceneView implements GameBoardView{
@@ -12,9 +15,20 @@ public class GameBoardViewImpl extends AbstractSceneView implements GameBoardVie
     private GridPane board;
 
     @Override
-    public void addCell(int width, int height, SlotType slotType) {
-        Label tmp = new Label();
-        tmp.setText(slotType.equals(SlotType.VOID) ? "void" : "altro");
-        this.board.add(tmp, width, height);
+    public void updatePlayer(String nickname, int coins, int money, Set<String> items) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updatePlayer'");
+    }
+
+    @Override
+    public void updateCommands(Set<String> items) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCommands'");
+    }
+
+    @Override
+    public void setUpBoard(int width, int height, Map<Position, SlotType> map, Set<String> nicknames) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setUpBoard'");
     }    
 }
