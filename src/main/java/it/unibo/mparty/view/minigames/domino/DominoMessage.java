ce76ae79435fb2609d1c0902bd8c5aca6c770180
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-01 11:28:17 +0200

    added turn message

--- src/main/java/it/unibo/mparty/view/minigames/domino/DominoMessage.java
+++ src/main/java/it/unibo/mparty/view/minigames/domino/DominoMessage.java
@@ -2,4 +2,16 @@ package it.unibo.mparty.view.minigames.domino;
 
 public enum DominoMessage {
 
+    TURN("E' il turno di ");
+
+    private final String message;
+
+    DominoMessage(final String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "*" + this.message;
+    }
 }
