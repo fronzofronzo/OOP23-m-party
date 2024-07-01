13bbac6ed11fab0376e84b037c26174d12eadfd4
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-01 17:28:04 +0200

    Implemented GameBoardView

--- src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardView.java
+++ src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardView.java
@@ -1,10 +1,16 @@
 package it.unibo.mparty.view.GameBoardView;
 
 import it.unibo.mparty.model.gameBoard.util.SlotType;
+import it.unibo.mparty.utilities.Position;
 import it.unibo.mparty.view.SceneView;
+import java.util.Set;
+import java.util.Map;
 
 public interface GameBoardView extends SceneView{
 
+    void updatePlayer(String nickname, int coins, int money, Set<String> items);
 
-    public void addCell(int width, int height, SlotType slotType);
+    void updateCommands(Set<String> items);
+
+    void setUpBoard(int width, int height, Map<Position,SlotType> map, Set<String> nicknames);
 }
