540d6862ab06b593506001e50a89f28b49424daf
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-01 16:43:38 +0200

    let MemorySweepViewImpl implement MemorySweepView

---
+++ src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepView.java
@@ -0,0 +1,5 @@
+package it.unibo.mparty.view.minigames.memorySweep;
+
+public interface MemorySweepView {
+
+}
---
+++ src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepViewImpl.java
@@ -0,0 +1,5 @@
+package it.unibo.mparty.view.minigames.memorySweep;
+
+public class MemorySweepViewImpl implements MemorySweepView {
+
+}
