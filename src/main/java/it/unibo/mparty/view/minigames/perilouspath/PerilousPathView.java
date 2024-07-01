4367f6ce154dcb7fb60585bda48f72ff21fb6b73
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-01 10:09:52 +0200

    changed one method name and added another method in PerilousPathView interface

--- src/main/java/it/unibo/mparty/view/minigames/perilouspath/PerilousPathView.java
+++ src/main/java/it/unibo/mparty/view/minigames/perilouspath/PerilousPathView.java
@@ -4,6 +4,7 @@ import it.unibo.mparty.controller.minigames.perilousPath.PerilousPathController;
 import it.unibo.mparty.model.minigames.perilouspath.api.AbstractPosition;
 import it.unibo.mparty.model.minigames.perilouspath.api.PerilousPath;
 import it.unibo.mparty.view.SceneView;
+import javafx.event.ActionEvent;
 
 import java.util.List;
 
@@ -16,5 +17,7 @@ public interface PerilousPathView  extends SceneView{
     public void hideBombs(List<AbstractPosition> bombs);
 
     public void hitTile(PerilousPath.Type type);
+
+    public void buttonClicked(ActionEvent e);
     
 }
