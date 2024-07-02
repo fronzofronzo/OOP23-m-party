57ecfac8a90a747035d5cd68533fe540094b3372
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-02 11:26:01 +0200

    Implemented FXML fields

--- src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardViewImpl.java
+++ src/main/java/it/unibo/mparty/view/GameBoardView/GameBoardViewImpl.java
@@ -8,7 +8,9 @@ import it.unibo.mparty.model.gameBoard.util.SlotType;
 import it.unibo.mparty.utilities.Position;
 import it.unibo.mparty.view.AbstractSceneView;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -18,16 +20,56 @@ public class GameBoardViewImpl extends AbstractSceneView implements GameBoardVie
     private final static int N_VBOX = 4;
 
     private GridPane board;
-    @FXML
-    private Pane paneCommand;
+    @FXML 
+    private BorderPane borderPane;
     @FXML 
     private VBox sectionP1;
     @FXML 
+    private Label nameP1;
+    @FXML 
+    private Label coinsP1;
+    @FXML 
+    private Label starsP1;
+    @FXML 
     private VBox sectionP2;
+    @FXML 
+    private Label nameP2;
+    @FXML 
+    private Label coinsP2;
+    @FXML 
+    private Label starsP2;
     @FXML 
     private VBox sectionP3;
     @FXML 
-    private VBox sectionP4; 
+    private Label nameP3;
+    @FXML 
+    private Label coinsP3;
+    @FXML 
+    private Label starsP3;
+    @FXML 
+    private VBox sectionP4;
+    @FXML 
+    private Label nameP4;
+    @FXML 
+    private Label coinsP4;
+    @FXML 
+    private Label starsP4;
+    @FXML
+    private Pane paneCommand;
+    @FXML 
+    private Button useItem1;
+    @FXML 
+    private Button useItem2;
+    @FXML 
+    private Button useItem3;
+    @FXML
+    private Button buttonUP;
+    @FXML
+    private Button buttonLEFT;
+    @FXML
+    private Button buttonDOWN;
+    @FXML
+    private Button buttonRIGHT;
 
     @Override
     public void updatePlayer(String nickname, int coins, int money, Set<String> items) {
