048bb8b0b7f977298b2d4b0f45a3d008fb2fa961
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-02 23:14:37 +0200

    implemented Board

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/api/Board.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/api/Board.java
@@ -2,11 +2,32 @@ package it.unibo.mparty.model.minigames.nanogram.api;
 
 import it.unibo.mparty.utilities.Position;
 
+/**
+ * Interface representing a board for a Nanogram game.
+ * This interface defines methods to manage the state of cells on the board.
+ */
 public interface Board {
 
+    /**
+     * Retrieves the size of the board.
+     *
+     * @return the size of the board (number of rows/columns).
+     */
     int getSize();
 
+    /**
+     * Sets the state of the cell at the specified position.
+     *
+     * @param position the position of the cell.
+     * @param state    the state to set for the cell (true for filled, false for empty).
+     */
     void setCellState(Position position, boolean state);
 
+    /**
+     * Retrieves the state of the cell at the specified position.
+     *
+     * @param position the position of the cell.
+     * @return true if the cell is filled, false if it is empty or the position is not found.
+     */
     boolean getState(Position position);
 }
--- src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/BoardImpl.java
@@ -6,28 +6,46 @@ import it.unibo.mparty.utilities.Position;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Implementation of the {@link Board} interface for managing the state of a Nanogram game board.
+ * This class uses a {@link Map} to track the state of each cell on the board.
+ */
 public class BoardImpl implements Board {
 
     protected final Map<Position, Boolean> board;
     private final int size;
 
+    /**
+     * Constructs a {@code BoardImpl} instance with the specified size.
+     *
+     * @param size the size of the board (size x size).
+     */
     public BoardImpl(final int size) {
         this.board = new HashMap<>();
         this.size = size;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getSize() {
         return this.size;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void setCellState(Position position, boolean state) {
+    public void setCellState(final Position position, final boolean state) {
         this.board.put(position, state);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean getState(Position position) {
+    public boolean getState(final Position position) {
         return this.board.getOrDefault(position, false);
     }
 }
