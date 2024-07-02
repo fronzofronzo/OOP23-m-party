d4b4ebed7ceca780d3f134fcb4473862415dce6b
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-03 00:27:11 +0200

    added the javadoc comments to the MemorySweepView interface

--- src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepView.java
+++ src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepView.java
@@ -7,13 +7,33 @@ import javafx.event.ActionEvent;
 
 import java.util.Set;
 
+/**
+ * interface that model the view that handles the events that happen when a button is clicked for the Memory Sweep mini-game
+ */
 public interface MemorySweepView {
 
+    /**
+     * method that sets up the view by marking the buttons randomly chosen
+     * @param randoms the group of randomly chosen positions
+     */
     public void setUp(Set<Position> randoms);
 
+    /**
+     * method that manages the aftermath of the clicked button,
+     * based on the parameter passed in input the view will be updated accordingly
+     * @param type the parameter on which depends the view update
+     */
     public void hit(MemorySweep.HitType type);
 
+    /**
+     * method that handles the click of a button in the grid
+     * @param e the event that needs to be handled
+     */
     public void buttonClicked(ActionEvent e);
 
+    /**
+     * links the controller of the mini-game to this view
+     * @param controller the controller that needs to be linked
+     */
     public void setObserver(MemorySweepController controller);
 }
