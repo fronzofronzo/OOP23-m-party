f3c20e03256fe9dbe45224b5466cd9367478e22a
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-02 17:24:58 +0200

    removed an unused import

--- src/main/java/it/unibo/mparty/model/minigames/connect4/impl/Connect4ModelImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/connect4/impl/Connect4ModelImpl.java
@@ -3,7 +3,6 @@ package it.unibo.mparty.model.minigames.connect4.impl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.stream.IntStream;
 
 import it.unibo.mparty.model.gameBoard.util.Pair;
