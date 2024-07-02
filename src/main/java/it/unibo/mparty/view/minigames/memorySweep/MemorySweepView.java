2fe84f14cebb891667e3907d6484f8abc60e6b17
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-02 10:21:57 +0200

    starting to implement the methods in MemorySweepViewImpl class

--- src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepView.java
+++ src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepView.java
@@ -1,5 +1,17 @@
 package it.unibo.mparty.view.minigames.memorySweep;
 
+import it.unibo.mparty.controller.minigames.memorySweep.MemorySweepController;
+import it.unibo.mparty.model.minigames.memorysweep.api.MemorySweep;
+import it.unibo.mparty.utilities.Position;
+import javafx.event.ActionEvent;
+
 public interface MemorySweepView {
 
+    public void setUp();
+
+    public void hit(MemorySweep.HitType type);
+
+    public void buttonClicked(ActionEvent e);
+
+    public void setObserver(MemorySweepController controller);
 }
--- src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepViewImpl.java
+++ src/main/java/it/unibo/mparty/view/minigames/memorySweep/MemorySweepViewImpl.java
@@ -1,5 +1,36 @@
 package it.unibo.mparty.view.minigames.memorySweep;
 
+import it.unibo.mparty.controller.minigames.memorySweep.MemorySweepController;
+import it.unibo.mparty.model.minigames.memorysweep.api.MemorySweep;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class MemorySweepViewImpl implements MemorySweepView {
 
+    private MemorySweepController observer;
+
+    @FXML
+    private GridPane memorySweepGrid;
+
+    @Override
+    public void setUp() {
+
+    }
+
+    @Override
+    public void hit(MemorySweep.HitType type) {
+
+    }
+
+    @Override
+    public void buttonClicked(ActionEvent e) {
+
+    }
+
+    @Override
+    public void setObserver(MemorySweepController controller) {
+        this.observer = controller;
+
+    }
 }
