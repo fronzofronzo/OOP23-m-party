174bfdb7aaa0ae5d55e27b544800bded7cf979e5
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-02 17:15:55 +0200

    added some methods to the model

--- src/main/java/it/unibo/mparty/model/minigames/connect4/api/Connect4Model.java
+++ src/main/java/it/unibo/mparty/model/minigames/connect4/api/Connect4Model.java
@@ -3,5 +3,7 @@ package it.unibo.mparty.model.minigames.connect4.api;
 import it.unibo.mparty.model.minigames.MinigameModel;
 
 public interface Connect4Model extends MinigameModel{
-    
+    public String getTurnPlayer();
+
+    public boolean addDisc(int column);
 }
--- src/main/java/it/unibo/mparty/model/minigames/connect4/impl/Connect4ModelImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/connect4/impl/Connect4ModelImpl.java
@@ -1,9 +1,14 @@
 package it.unibo.mparty.model.minigames.connect4.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
 
 import it.unibo.mparty.model.gameBoard.util.Pair;
 import it.unibo.mparty.model.minigames.connect4.api.Connect4Model;
+import it.unibo.mparty.utilities.Position;
 
 public class Connect4ModelImpl implements Connect4Model{
 
@@ -11,6 +16,8 @@ public class Connect4ModelImpl implements Connect4Model{
     private final static int COLUMN_NUMBER = 8;
     private String player1;
     private String player2;
+    private String turnPlayer;
+    private Map<String,Position> selectedMap = new HashMap<>();
 
     @Override
     public boolean isOver() {
@@ -29,5 +36,46 @@ public class Connect4ModelImpl implements Connect4Model{
         player1 = players.get(0);
         player2 = players.get(1);
     }
+
+    private boolean isADraw() {
+        return selectedMap.size()>=(ROW_NUMBER * COLUMN_NUMBER);
+    }
+
+    @Override
+    public String getTurnPlayer() {
+        return this.turnPlayer;
+    }
+
+    @Override
+    public boolean addDisc(int column) {
+        int row = getAvailableRow(column);
+        if (row ==-1) {
+            return false;
+        }
+        manageTurn();
+        return true;
+    }
+
+    private int getAvailableRow (int column) {
+        try {
+            return IntStream.range(0, ROW_NUMBER)
+            .filter(i -> !(selectedMap.values().contains(new Position(i,column))))
+            .findFirst()
+            .getAsInt();
+        } catch (NoSuchElementException e) {
+            return -1;
+        }
+    }
+
+    private void manageTurn () {
+        if (getTurnPlayer()==player1) {
+            turnPlayer=player2;
+        }
+        else {
+            turnPlayer=player1;
+        }
+    }
+
+    
 
 }
