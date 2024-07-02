b851e19674217416bb19badb7934f54832a446c3
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-02 14:44:58 +0200

    Modified javadoc of DiceImpl class

--- src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
+++ src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
@@ -4,6 +4,10 @@ import it.unibo.mparty.model.player.api.Dice;
 
 import java.util.Random;
 
+/**
+ * This class models the implementation of {@link Dice}. Each dice at first
+ * is initialized with standard parameters and can be modified
+ */
 public class DiceImpl implements Dice {
 
     private static final int ATTEMPTS = 1;
@@ -15,30 +19,45 @@ public class DiceImpl implements Dice {
     private int minNumber = MIN_NUMBER;
     private Random random;
 
+    /**
+     * Constructor of a new dice. It initialises the random variable
+     */
     public DiceImpl(){
         this.random = new Random();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setNumberOfAttempts(int num) {
         this.numberOfAttempts = num;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setMaxNumber(int num) {
         this.maxNumber = num;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setMinNumber(int num) {
         this.minNumber = num;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int generateNumber() {
         int number = 0;
         for(int i = 0; i < numberOfAttempts; i++){
-            number +=  random.nextInt(minNumber,maxNumber+1);
+            number += random.nextInt(minNumber,maxNumber+1);
         }
         return number;
     }
