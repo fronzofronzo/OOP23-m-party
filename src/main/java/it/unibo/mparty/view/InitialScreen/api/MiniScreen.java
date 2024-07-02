0b511771b732869cc02d43716fbb160f1794632f
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-03 00:18:41 +0200

    added the javadoc for the MiniScreen interface

--- src/main/java/it/unibo/mparty/view/InitialScreen/api/MiniScreen.java
+++ src/main/java/it/unibo/mparty/view/InitialScreen/api/MiniScreen.java
@@ -3,16 +3,38 @@ package it.unibo.mparty.view.InitialScreen.api;
 import it.unibo.mparty.model.GameModelBuilder;
 import javafx.event.ActionEvent;
 
+/**
+ * interface that models the controller for the MiniScreen controller that opens up when you click the add player button
+ */
 public interface MiniScreen {
-
+    /**
+     * method that handles the event that happens when you click the ok button
+     * @param e event that happens when you click the start button
+     */
     void handleOkButton(ActionEvent e);
 
+    /**
+     * method that handles the event that happens when you click the back button
+     * @param e event that happens when you click the back button
+     */
     void handleBackButton(ActionEvent e);
 
+    /**
+     * method that handles the event that happens when you click the choice box
+     * @param e event that happens when you click the choice box
+     */
     void handleCharacterChoiceBox(ActionEvent e);
 
+    /**
+     * method that handles the event that happens when you write in the text field area
+     * @param e event that happens when you start writing in the text field area
+     */
     void handleUsernameTextField(ActionEvent e);
 
+    /**
+     *  method that attaches a GameModelBuilder object to the view
+     * @param builder an object that the view calls when it is needed to create the Game model
+     */
     void setUp(GameModelBuilder builder);
 
 
