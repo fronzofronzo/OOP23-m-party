7c376c004e5e7df623ed6882de3badc443edb4b3
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-02 11:33:20 +0200

    refactored NanogramModel

--- src/main/java/it/unibo/mparty/model/minigames/nanogram/api/NanogramModel.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/api/NanogramModel.java
@@ -1,97 +1,24 @@
 package it.unibo.mparty.model.minigames.nanogram.api;
 
-import it.unibo.mparty.model.minigames.nanogram.util.CellState;
-import it.unibo.mparty.model.minigames.nanogram.util.Difficulty;
-
 import java.util.List;
 
-/**
- * Interface representing the model for a Nanogram game.
- * This interface defines methods to interact with the game state and rules.
- */
 public interface NanogramModel {
 
-    /**
-     * Initializes the game grid based on the given difficulty.
-     *
-     * @param difficulty the difficulty level to set up the game.
-     */
-    void initializeGame(Difficulty difficulty);
+    boolean getSolutionCellState(int x, int y);
 
-    /**
-     * Gets the state of a specific cell in the grid.
-     *
-     * @param row    the row index of the cell.
-     * @param column the column index of the cell.
-     * @return the current state of the cell at the specified position.
-     */
-    CellState getCellState(int row, int column);
-
-    /**
-     * Gets the current board of the game.
-     *
-     * @return the current {@link Board} of the game.
-     */
-    Board getBoard();
-
-    /**
-     * Gets the number of lives remaining in the game.
-     *
-     * @return the number of remaining lives.
-     */
     int getLives();
 
-    /**
-     * Updates the state of a specific cell in the grid.
-     *
-     * @param row   the row index of the cell.
-     * @param column the column index of the cell.
-     * @param state the new state to set for the cell.
-     */
-    void updateCellState(int row, int column, CellState state);
+    void hitCell(int x, int y, boolean state);
 
-    /**
-     * Updates the number of lives remaining in the game.
-     *
-     * @param lives the new number of lives.
-     */
+    boolean isMoveValid(int x, int y, boolean state);
+
     void updateLives(int lives);
 
-    /**
-     * Gets the list of hints for each row of the grid.
-     *
-     * @return a list of lists of integers representing the row hints.
-     */
     List<List<Integer>> getRowHints();
 
-    /**
-     * Gets the list of hints for each column of the grid.
-     *
-     * @return a list of lists of integers representing the column hints.
-     */
     List<List<Integer>> getColumnHints();
 
-    /**
-     * Checks if a move with the given state at the specified position is valid.
-     *
-     * @param row        the row index of the cell.
-     * @param column     the column index of the cell.
-     * @param actualState the state to be validated.
-     * @return true if the move is valid, false otherwise.
-     */
-    boolean isMoveValid(int row, int column, CellState actualState);
-
-    /**
-     * Checks if the game is complete.
-     *
-     * @return true if the player has successfully completed the game, false otherwise.
-     */
     boolean isGameComplete();
 
-    /**
-     * Checks if the game is over, either by completing the game or losing all lives.
-     *
-     * @return true if the game is over, false otherwise.
-     */
     boolean isGameOver();
 }
--- src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/NanogramModelImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/nanogram/impl/NanogramModelImpl.java
@@ -1,128 +1,78 @@
 package it.unibo.mparty.model.minigames.nanogram.impl;
 
 import it.unibo.mparty.model.minigames.nanogram.api.Board;
-import it.unibo.mparty.model.minigames.nanogram.api.BoardFactory;
 import it.unibo.mparty.model.minigames.nanogram.api.Live;
 import it.unibo.mparty.model.minigames.nanogram.api.NanogramModel;
-import it.unibo.mparty.model.minigames.nanogram.util.CellState;
-import it.unibo.mparty.model.minigames.nanogram.util.Difficulty;
+import it.unibo.mparty.model.minigames.nanogram.api.SimpleBoard;
 import it.unibo.mparty.utilities.Position;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.IntStream;
 
-/**
- * Implementation of the {@link NanogramModel} interface representing the model for a Nanogram game.
- */
 public class NanogramModelImpl implements NanogramModel {
 
-    private final Map<Position, CellState> grid = new HashMap<>();
-    private final BoardFactory boardFactory = new BoardFactoryImpl();
-    private final Live lives = new LiveImpl();
+    private static final int SIZE_SIMPLE_BOARD = 5;
+    private static final int SIMPLE_FILL_PERCENTAGE = 60;
+    private final Live lives;
+    private final List<List<Integer>> rowHints;
+    private final List<List<Integer>> columnHints;
+    private final SimpleBoard solutionBoard;
+    private final Board hittedBoard;
 
-    private List<List<Integer>> rowHints = new ArrayList<>();
-    private List<List<Integer>> columnHints = new ArrayList<>();
-    private Board board;
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void initializeGame(final Difficulty difficulty) {
-        if (difficulty == Difficulty.SIMPLE) {
-            this.board = this.boardFactory.createSimpleBoard(5, 60);
-        } else {
-            this.board = this.boardFactory.createHardBoard(10, 40, 20);
-        }
-        this.rowHints = this.board.calculateHints(true);
-        this.columnHints = this.board.calculateHints(false);
+    public NanogramModelImpl() {
+        this.lives = new LiveImpl();
+        this.solutionBoard = new SimpleBoardImpl(SIZE_SIMPLE_BOARD, SIMPLE_FILL_PERCENTAGE);
+        this.hittedBoard = new BoardImpl(SIZE_SIMPLE_BOARD);
+        this.rowHints = this.solutionBoard.getHints(true);
+        this.columnHints = this.solutionBoard.getHints(false);
         this.lives.reset();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public CellState getCellState(final int row, final int column) {
-        return this.board.getGrid().get(new Position(row, column));
+    public boolean getSolutionCellState(final int x, final int y) {
+        return this.solutionBoard.getState(new Position(x, y));
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Board getBoard() {
-        return this.board;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public int getLives() {
         return this.lives.getLive();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void updateCellState(final int row, final int column, final CellState state) {
-        if (isMoveValid(row, column, state)) {
-            this.grid.put(new Position(row, column), state);
-        } else {
-            updateLives(lives.getLive() - 1);
-            this.grid.compute(new Position(row, column), (k, correctState) -> correctState);
-        }
+    public void hitCell(final int x, final int y, final boolean state) {
+        this.hittedBoard.setCellState(new Position(x, y), state);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    @Override
+    public boolean isMoveValid(final int x, final int y, final boolean state) {
+        Position position = new Position(x, y);
+        return hittedBoard.getCellState(position) == solutionBoard.getState(position);
+    }
+
     @Override
     public void updateLives(final int lives) {
         this.lives.update(lives);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isMoveValid(final int row, final int column, final CellState state) {
-        return state.equals(this.board.getGrid().get(new Position(row, column)));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public List<List<Integer>> getRowHints() {
         return this.rowHints;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public List<List<Integer>> getColumnHints() {
         return this.columnHints;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public boolean isGameComplete() {
-        return board.getGrid().entrySet().stream()
-                .filter(e -> e.getValue().equals(CellState.FILLED))
-                .allMatch(e -> CellState.FILLED.equals(this.grid.get(e.getKey())));
+        return IntStream.range(0, SIZE_SIMPLE_BOARD)
+                .boxed().flatMap(row -> IntStream.range(0, SIZE_SIMPLE_BOARD)
+                        .mapToObj(col -> new Position(row, col)))
+                .filter(solutionBoard::getState)
+                .allMatch(hittedBoard::getCellState);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public boolean isGameOver() {
         return this.lives.isDeath();
