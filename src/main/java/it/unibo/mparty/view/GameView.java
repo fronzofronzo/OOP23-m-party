d1c5163b22e43a4f36b207116449d4462027aa1a
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-03 20:18:42 +0200

    Implemented method of game board view impl in game view

--- src/main/java/it/unibo/mparty/view/GameView.java
+++ src/main/java/it/unibo/mparty/view/GameView.java
@@ -2,6 +2,7 @@ package it.unibo.mparty.view;
 
 import java.io.IOException;
 
+import it.unibo.mparty.utilities.Direction;
 import it.unibo.mparty.utilities.Pair;
 import it.unibo.mparty.utilities.Position;
 import it.unibo.mparty.utilities.SlotType;
@@ -27,5 +28,11 @@ public interface GameView {
     /**
      * Switch to te board scene
      */
+    void showResultDice(int result);
 
+    void updatePlayerPos(String player, Position position);
+
+    void updatePlayerStats(String player, int coins, int stars, List<String> items);
+
+    void updateCommands(List<String> items, List<Direction> directions);
 }
