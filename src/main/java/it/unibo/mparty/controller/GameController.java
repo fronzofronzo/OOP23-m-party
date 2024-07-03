8ccaf905bee1ee2b8198f39c28d7df3d3a128c19
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-03 10:23:49 +0200

    Added javadoc to GameController interface and relative methods

--- src/main/java/it/unibo/mparty/controller/GameController.java
+++ src/main/java/it/unibo/mparty/controller/GameController.java
@@ -3,14 +3,36 @@ package it.unibo.mparty.controller;
 import it.unibo.mparty.model.GameModel;
 import it.unibo.mparty.model.item.impl.ItemName;
 
+/**
+ * This interface models the Controller ( pattern MVC ) of the main game. After the game
+ * initialisation, the controller starts the game and sets the model created
+ * during the initialisation phase. It also handles user request to the model
+ */
 public interface GameController {
 
+    /**
+     * Handle the request of the user to roll dice
+     * @return the result of rolling dice
+     */
     int rollDice();
 
+    /**
+     * Handle the action of moving the player in the {@code GameBoard }
+     */
     void movePlayer();
 
+    /**
+     * Manage the acquisition of an item of the player's playing the
+     * turn
+     * @param item that player wants to buy
+     * @return true if the player is able to buy it, false otherwise
+     */
     boolean buyItem(ItemName item);
 
+    /**
+     * Manage the start of a new game
+     * @param model to set, created during the initial game phase
+     */
     void startGame(GameModel model);
 
     void endGame();
