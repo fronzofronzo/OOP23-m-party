fa5bd2b3c696d4e8ec41d3b8c3571619bf274517
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-03 12:31:27 +0200

    modified declaretion in the controller impl

--- src/main/java/it/unibo/mparty/controller/minigames/connect4/impl/Connect4ControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/connect4/impl/Connect4ControllerImpl.java
@@ -1,5 +1,22 @@
 package it.unibo.mparty.controller.minigames.connect4.impl;
 
-public class Connect4ControllerImpl {
+import java.util.List;
+
+import it.unibo.mparty.controller.minigames.MinigameController;
+import it.unibo.mparty.controller.minigames.connect4.api.Connect4Controller;
+
+public class Connect4ControllerImpl implements MinigameController, Connect4Controller {
+
+    @Override
+    public void endGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'endGame'");
+    }
+
+    @Override
+    public void initGame(List<String> players) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initGame'");
+    }
 
 }
