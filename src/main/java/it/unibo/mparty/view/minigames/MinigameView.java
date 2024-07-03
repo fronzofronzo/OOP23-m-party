effd20e6dd737acdad9d01aa7af54663ed438402
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-03 09:50:29 +0200

    added methods to minigame view interface

--- src/main/java/it/unibo/mparty/view/minigames/MinigameView.java
+++ src/main/java/it/unibo/mparty/view/minigames/MinigameView.java
@@ -7,4 +7,14 @@ import it.unibo.mparty.view.SceneView;
  * This interface extends the {@link SceneView} interface to include functionality specific to minigames.
  */
 public interface MinigameView extends SceneView {
+
+    /**
+     * Method to set up the view when the game stars
+     */
+    void setUpView();
+
+    /**
+     * Method to show the result of the minigame
+     */
+    void showResult();
 }
