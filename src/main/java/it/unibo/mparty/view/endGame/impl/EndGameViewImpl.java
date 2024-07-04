186357989e664adc51b27193a5db4475208972c0
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-04 18:53:06 +0200

    update endScene.fxml

--- src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
+++ src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
@@ -1,6 +1,31 @@
 package it.unibo.mparty.view.endGame.impl;
 
+import it.unibo.mparty.view.AbstractSceneView;
 import it.unibo.mparty.view.endGame.api.EndGameView;
 
-public class EndGameViewImpl implements EndGameView {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseEvent;
+
+public class EndGameViewImpl extends AbstractSceneView implements EndGameView {
+    @FXML
+    private TableColumn<?, ?> coinColumn;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private TableColumn<?, ?> playerColumn;
+
+    @FXML
+    private TableColumn<?, ?> rankColumn;
+
+    @FXML
+    private TableColumn<?, ?> starColumn;
+
+    @FXML
+    private void menuClicked(MouseEvent event) {
+
+    }
 }
