ea8084d6d3d4756899c403dcf4d20396bc692eef
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-04 11:47:27 +0200

    deleted turn message

--- src/main/java/it/unibo/mparty/view/minigames/domino/DominoMessage.java
+++ src/main/java/it/unibo/mparty/view/minigames/domino/DominoMessage.java
@@ -2,13 +2,13 @@ package it.unibo.mparty.view.minigames.domino;
 
 public enum DominoMessage {
 
-    TURN("E' il turno di "),
-
     MOVE_NOT_VALID("Mossa non valida. Seleziona un'altra tessera."),
 
     DRAW_TILE("Tessere non compattibili. Pesca una tessera."),
 
-    WIN(" ha vinto!");
+    WIN(" ha vinto!"),
+
+    SELECT_TILE("Seleziona una tessera per giocare.");
 
     private final String message;
 
