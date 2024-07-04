4a5cade2c7e0be3c7741e1148ec381a22bab28ca
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-04 11:30:29 +0200

    corrected BoardTileImplTest

--- src/test/java/it/unibo/mparty/model/minigames/domino/impl/BoardTileImplTest.java
+++ src/test/java/it/unibo/mparty/model/minigames/domino/impl/BoardTileImplTest.java
@@ -26,7 +26,7 @@ class BoardTileImplTest {
     }
 
     @Test
-    void canPlaceTile() {
+    void canMatchBoardTile() {
         Tile tile1 = new TileImpl(SIDE3, SIDE5);
         Tile tile2 = new TileImpl(SIDE5, SIDE2);
         Tile tile3 = new TileImpl(SIDE2, SIDE4);
@@ -34,28 +34,32 @@ class BoardTileImplTest {
         Tile tile5 = new TileImpl(SIDE1, SIDE4);
 
         // Test placing the first tile on an empty board
-        assertTrue(boardTile.canPlaceTile(tile1));
-        assertEquals(1, boardTile.getBoardTiles().size());
-        assertTrue(boardTile.getBoardTiles().contains(tile1));
+        assertTrue(this.boardTile.canMatchBoardTile(tile1));
+        this.boardTile.addTileToBoard(tile1);
+        assertEquals(1, this.boardTile.getBoardTiles().size());
+        assertTrue(this.boardTile.getBoardTiles().contains(tile1));
 
         // Test placing a matching tile on the right end
-        assertTrue(boardTile.canPlaceTile(tile2));
-        assertEquals(2, boardTile.getBoardTiles().size());
-        assertTrue(boardTile.getBoardTiles().contains(tile2));
+        assertTrue(this.boardTile.canMatchBoardTile(tile2));
+        this.boardTile.addTileToBoard(tile2);
+        assertEquals(2, this.boardTile.getBoardTiles().size());
+        assertTrue(this.boardTile.getBoardTiles().contains(tile2));
 
         // Test placing a matching tile on the left end
-        assertTrue(boardTile.canPlaceTile(tile4));
-        assertEquals(3, boardTile.getBoardTiles().size());
-        assertTrue(boardTile.getBoardTiles().contains(tile4));
+        assertTrue(this.boardTile.canMatchBoardTile(tile4));
+        this.boardTile.addTileToBoard(tile4);
+        assertEquals(3, this.boardTile.getBoardTiles().size());
+        assertTrue(this.boardTile.getBoardTiles().contains(tile4));
 
         // Test placing a non-matching tile
-        assertFalse(boardTile.canPlaceTile(tile5));
-        assertEquals(3, boardTile.getBoardTiles().size());
-        assertFalse(boardTile.getBoardTiles().contains(tile5));
+        assertFalse(this.boardTile.canMatchBoardTile(tile5));
+        assertEquals(3, this.boardTile.getBoardTiles().size());
+        assertFalse(this.boardTile.getBoardTiles().contains(tile5));
 
         // Test placing another matching tile on the right end
-        assertTrue(boardTile.canPlaceTile(tile3));
-        assertEquals(4, boardTile.getBoardTiles().size());
-        assertTrue(boardTile.getBoardTiles().contains(tile3));
+        assertTrue(this.boardTile.canMatchBoardTile(tile3));
+        this.boardTile.addTileToBoard(tile3);
+        assertEquals(4, this.boardTile.getBoardTiles().size());
+        assertTrue(this.boardTile.getBoardTiles().contains(tile3));
     }
 }
