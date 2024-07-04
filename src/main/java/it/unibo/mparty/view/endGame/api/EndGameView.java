7b40325bcf21326b28e25ac4eba668a08004bfc7
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-04 12:12:57 +0200

    initialize EndGameView

---
+++ src/main/java/it/unibo/mparty/view/endGame/api/EndGameView.java
@@ -0,0 +1,4 @@
+package it.unibo.mparty.view.endGame.api;
+
+public interface EndGameView {
+}
---
+++ src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
@@ -0,0 +1,6 @@
+package it.unibo.mparty.view.endGame.impl;
+
+import it.unibo.mparty.view.endGame.api.EndGameView;
+
+public class EndGameViewImpl implements EndGameView {
+}
