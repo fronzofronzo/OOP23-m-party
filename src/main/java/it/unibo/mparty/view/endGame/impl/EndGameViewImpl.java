285030a42c9ad2c40ccbe481a447d2c20518783d
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-05 17:16:09 +0200

    implemented EndGameViewImpl

--- src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
+++ src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
@@ -5,9 +5,10 @@ import it.unibo.mparty.view.AbstractSceneView;
 import it.unibo.mparty.view.endGame.api.EndGameView;
 
 import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.List;
@@ -18,30 +19,38 @@ public class EndGameViewImpl extends AbstractSceneView implements EndGameView {
     private TableColumn<Player, Integer> coinColumn;
 
     @FXML
-    private Button exitButton;
-
-    @FXML
     private TableColumn<Player, String> playerColumn;
 
     @FXML
-    private TableColumn<?, ?> rankColumn;
+    private TableColumn<Player, Integer> rankColumn;
 
     @FXML
     private TableColumn<Player, Integer> starColumn;
 
+    @FXML
+    private TableView<Player> tableView;
+
     @Override
     public void setPlayerColumn(List<String> playerNames){
-        this.playerColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        for (String playerName : playerNames) {
+            this.playerColumn.setCellValueFactory(new PropertyValueFactory<>(playerName));
+        }
+        this.rankColumn.setCellValueFactory(cellData ->
+                new ReadOnlyObjectWrapper<>(tableView.getItems().indexOf(cellData.getValue()) + 1));
     }
 
     @Override
     public void setCoinColumn(List<Integer> playerCoins){
-
+        for (Integer coin : playerCoins) {
+            this.coinColumn.setCellValueFactory(new PropertyValueFactory<>(String.valueOf(coin)));
+        }
     }
 
     @Override
     public void setStarColumn(List<Integer> playerStars){
-
+        for (Integer star : playerStars) {
+            this.starColumn.setCellValueFactory(new PropertyValueFactory<>(String.valueOf(star)));
+        }
     }
 
     @FXML
