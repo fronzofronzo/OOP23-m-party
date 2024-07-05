ea1410e0bf84bc306fb39588f7093a43f353712b
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-05 11:21:22 +0200

    Initialised DiceImplTest class with init method

--- src/test/java/it/unibo/mparty/model/Player/impl/DiceImplTest.java
+++ src/test/java/it/unibo/mparty/model/Player/impl/DiceImplTest.java
@@ -2,6 +2,20 @@ package it.unibo.mparty.model.Player.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import it.unibo.mparty.model.player.api.Dice;
+import it.unibo.mparty.model.player.impl.DiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Test class for a {@link DiceImpl} class
+ */
 class DiceImplTest {
+
+    private Dice testDice;
+
+    @BeforeEach
+    public void init(){
+        testDice = new DiceImpl();
+    }
 
 }
