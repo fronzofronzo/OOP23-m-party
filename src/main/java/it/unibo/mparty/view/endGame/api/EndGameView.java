95b95d75151dfef41c6ce1e120439c12e7263417
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-05 16:29:24 +0200

    set up EndGameView

--- src/main/java/it/unibo/mparty/view/endGame/api/EndGameView.java
+++ src/main/java/it/unibo/mparty/view/endGame/api/EndGameView.java
@@ -1,4 +1,12 @@
 package it.unibo.mparty.view.endGame.api;
 
+import java.util.List;
+
 public interface EndGameView {
+
+    void setPlayerColumn(List<String> playerNames);
+
+    void setCoinColumn(List<Integer> playerCoins);
+
+    void setStarColumn(List<Integer> playerStars);
 }
--- src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
+++ src/main/java/it/unibo/mparty/view/endGame/impl/EndGameViewImpl.java
@@ -1,31 +1,51 @@
 package it.unibo.mparty.view.endGame.impl;
 
+import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.view.AbstractSceneView;
 import it.unibo.mparty.view.endGame.api.EndGameView;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class EndGameViewImpl extends AbstractSceneView implements EndGameView {
-    @FXML
-    private TableColumn<?, ?> coinColumn;
 
     @FXML
-    private Button menuButton;
+    private TableColumn<Player, Integer> coinColumn;
 
     @FXML
-    private TableColumn<?, ?> playerColumn;
+    private Button exitButton;
+
+    @FXML
+    private TableColumn<Player, String> playerColumn;
 
     @FXML
     private TableColumn<?, ?> rankColumn;
 
     @FXML
-    private TableColumn<?, ?> starColumn;
+    private TableColumn<Player, Integer> starColumn;
+
+    @Override
+    public void setPlayerColumn(List<String> playerNames){
+        this.playerColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+    }
+
+    @Override
+    public void setCoinColumn(List<Integer> playerCoins){
+
+    }
+
+    @Override
+    public void setStarColumn(List<Integer> playerStars){
+
+    }
 
     @FXML
-    private void menuClicked(MouseEvent event) {
-
+    private void exitClicked() {
+        Platform.exit();
     }
 }
