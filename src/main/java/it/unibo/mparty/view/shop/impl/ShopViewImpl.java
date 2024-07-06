58849e30abdaaf1f5780b12a8e5ca03021f222cc
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-06 15:56:58 +0200

    modified javadoc and style error

--- src/main/java/it/unibo/mparty/view/shop/impl/ShopViewImpl.java
+++ src/main/java/it/unibo/mparty/view/shop/impl/ShopViewImpl.java
@@ -12,13 +12,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-
-public class ShopViewImpl extends AbstractSceneView implements ShopView{
-
-    private int i=1;
-    private int j=1;
+/**
+ * Implements the view for the shop.
+ */
+public class ShopViewImpl extends AbstractSceneView implements ShopView {
+    private int i = 1;
+    private int j = 1;
     private List<ItemName> itemNameList = new ArrayList<>();
-    private final static double DISTANCE_FROM_BUTTON = 70.0;
+    private static final double DISTANCE_FROM_BUTTON = 70.0;
 
     @FXML
     private AnchorPane descriptionPane;
@@ -32,35 +33,47 @@ public class ShopViewImpl extends AbstractSceneView implements ShopView{
     @FXML 
     private Label moneyLabel;
 
-
-    public void addButton (ItemName itemName, int cost)
-    {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addButton(ItemName itemName, int cost) {
         final Button but = new Button();
-        but.setText("Acquista "+ itemName.toString() + ": " + cost);
+        but.setText("Acquista " + itemName.toString() + ": " + cost);
         but.setOnAction(this::selectItem);
         buttonPane.getChildren().add(but);
-        AnchorPane.setTopAnchor(but, i*DISTANCE_FROM_BUTTON);
+        AnchorPane.setTopAnchor(but, i * DISTANCE_FROM_BUTTON);
         i++;
         itemNameList.add(itemName);
     }
 
-    public void addDescription(String description)
-    {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addDescription(String description) {
         final Label lb = new Label(description);
         descriptionPane.getChildren().add(lb);
-        AnchorPane.setTopAnchor(lb,j*DISTANCE_FROM_BUTTON);
+        AnchorPane.setTopAnchor(lb, j * DISTANCE_FROM_BUTTON);
         j++;
     }
 
     private void selectItem(ActionEvent e) {
-        this.getMainController().buyItem(itemNameList.get(this.buttonPane.getChildren().indexOf((Button)(e.getSource()))),this);
+        this.getMainController().buyItem(itemNameList
+        .get(this.buttonPane.getChildren().indexOf((Button) (e.getSource()))), this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void updateMoney(int money) {
         moneyLabel.setText("Monete rimaste: " + money);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void initShopView() {
         this.getMainController().setUpShop(this);
