54f9e477639b7cb3ea17b9ef713dd6ea26114acb
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-06 22:50:28 +0200

    added javadoc for TileFactory

--- src/main/java/it/unibo/mparty/model/minigames/domino/api/TileFactory.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.mparty.model.minigames.domino.api;
-
-import java.util.List;
-
-public interface TileFactory {
-    List<Tile> createDoubleSixSet();
-}
--- src/main/java/it/unibo/mparty/model/minigames/domino/tile/impl/TileFactoryImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/domino/tile/impl/TileFactoryImpl.java
@@ -1,7 +1,7 @@
-package it.unibo.mparty.model.minigames.domino.impl;
+package it.unibo.mparty.model.minigames.domino.tile.impl;
 
-import it.unibo.mparty.model.minigames.domino.api.Tile;
-import it.unibo.mparty.model.minigames.domino.api.TileFactory;
+import it.unibo.mparty.model.minigames.domino.tile.api.Tile;
+import it.unibo.mparty.model.minigames.domino.tile.api.TileFactory;
 
 import java.util.List;
 import java.util.LinkedList;
@@ -9,8 +9,15 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Implementation of the {@link TileFactory} interface. Creates sets of domino tiles.
+ */
 public class TileFactoryImpl implements TileFactory {
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Tile> createDoubleSixSet() {
         List<Tile> tiles = IntStream.rangeClosed(0, 6).boxed()
