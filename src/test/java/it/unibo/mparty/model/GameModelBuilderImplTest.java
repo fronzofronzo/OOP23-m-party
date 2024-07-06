f40fcd46601983c00387e16bb6f53c7b0b6e72bb
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-06 16:42:43 +0200

    Implemented test for players number and modified GameBuilder implementation

--- src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
+++ src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
@@ -1,6 +1,10 @@
 package it.unibo.mparty.model;
 
+import it.unibo.mparty.model.player.api.Player;
+import it.unibo.mparty.model.player.impl.Character;
+import it.unibo.mparty.model.player.impl.PlayerImplementation;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +18,18 @@ class GameModelBuilderImplTest {
     @BeforeEach
     public void init(){
         this.builder = new GameModelBuilderImpl();
+    }
+
+    @Test
+    public void testNumberOfPlayers(){
+        final String user1 = "user1";
+        final String charact1 = Character.CHAR_MARIO.getName();
+        this.builder = this.builder.addPlayer(user1,charact1);
+        assertFalse(this.builder.enoughPlayers());
+        final String user2 = "user2";
+        final String charact2 = Character.CHAR_DK.getName();
+        this.builder = this.builder.addPlayer(user2,charact2);
+        assertTrue(this.builder.enoughPlayers());
     }
 
 }
