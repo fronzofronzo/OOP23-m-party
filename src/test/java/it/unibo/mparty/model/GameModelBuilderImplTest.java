5ace11e144101c786db2a33f1a326785ebc33b5d
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-06 17:21:04 +0200

    Implemented method to test building of model and corrected problems in implementations

--- src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
+++ src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
@@ -3,6 +3,7 @@ package it.unibo.mparty.model;
 import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.impl.Character;
 import it.unibo.mparty.model.player.impl.PlayerImplementation;
+import it.unibo.mparty.utilities.BoardType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -61,6 +62,21 @@ class GameModelBuilderImplTest {
                 () -> {this.builder.addPlayer(user2,charact2);});
     }
 
-
+    @Test
+    public void testBuildingModel(){
+        final String user1 = "user1";
+        final String charact1 = Character.CHAR_MARIO.getName();
+        final String user2 = "user2";
+        final String charact2 = Character.CHAR_DK.getName();
+        final GameModel model = this.builder.addPlayer(user1,charact1)
+                .addPlayer(user2,charact2)
+                .difficulty(BoardType.EASY.toString()).build();
+        assertTrue(model.getPlayers().stream()
+                .map(Player::getUsername)
+                .toList().contains(user1));
+        assertTrue(model.getPlayers().stream()
+                .map(Player::getUsername)
+                .toList().contains(user2));
+    }
 
 }
