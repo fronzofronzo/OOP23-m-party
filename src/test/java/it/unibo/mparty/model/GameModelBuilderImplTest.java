68afce45375febca4ffc91c528517e72c11c3ee1
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-06 16:32:07 +0200

    Implemented init method and added javadoc

--- src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
+++ src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
@@ -1,7 +1,19 @@
 package it.unibo.mparty.model;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for {@link GameModelBuilderImpl} class
+ */
 class GameModelBuilderImplTest {
+
+    private GameModelBuilder builder;
+
+    @BeforeEach
+    public void init(){
+        this.builder = new GameModelBuilderImpl();
+    }
 
 }
