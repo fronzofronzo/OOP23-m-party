be580bd97800c047a94437527e3bbf867a2536f7
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-06 23:19:43 +0200

    added final modifier and this keyword for DominoFactoryImplTest

--- src/test/java/it/unibo/mparty/model/minigames/domino/impl/DominoFactoryImplTest.java
+++ src/test/java/it/unibo/mparty/model/minigames/domino/impl/DominoFactoryImplTest.java
@@ -1,6 +1,8 @@
 package it.unibo.mparty.model.minigames.domino.impl;
 
-import it.unibo.mparty.model.minigames.domino.api.Tile;
+import it.unibo.mparty.model.minigames.domino.tile.api.Tile;
+import it.unibo.mparty.model.minigames.domino.tile.impl.TileFactoryImpl;
+import it.unibo.mparty.model.minigames.domino.tile.impl.TileImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +10,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DominoFactoryImplTest {
 
     private static final int DOMINO_SET_SIZE = 28;
     private TileFactoryImpl dominoFactory;
-
 
     @BeforeEach
     void setUp() {
@@ -23,15 +25,15 @@ class DominoFactoryImplTest {
 
     @Test
     void testCreateFullSet() {
-        List<Tile> fullSet = this.dominoFactory.createDoubleSixSet();
-        Set<Tile> expectedSet = generateFullSet();
+        final List<Tile> fullSet = this.dominoFactory.createDoubleSixSet();
+        final Set<Tile> expectedSet = this.generateFullSet();
 
         assertEquals(DOMINO_SET_SIZE, this.dominoFactory.createDoubleSixSet().size());
         assertTrue(fullSet.containsAll(expectedSet));
     }
 
     private Set<Tile> generateFullSet() {
-        Set<Tile> fullSet = new HashSet<>();
+        final Set<Tile> fullSet = new HashSet<>();
         for (int i = 0; i <= 6; i++) {
             for (int j = i; j <= 6; j++) {
                 fullSet.add(new TileImpl(i, j));
@@ -39,5 +41,4 @@ class DominoFactoryImplTest {
         }
         return fullSet;
     }
-
 }
