45dc65633bf0b7ce0ff5e31b41bae921ea655899
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-07 18:43:15 +0200

    Added method to test if an Item is added to the bag

--- src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
+++ src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
@@ -1,11 +1,15 @@
 package it.unibo.mparty.model.Player.impl;
 
+import it.unibo.mparty.model.item.api.Item;
+import it.unibo.mparty.model.item.impl.ItemFactoryImpl;
+import it.unibo.mparty.model.item.impl.ItemName;
 import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.api.PlayerBag;
 import it.unibo.mparty.model.player.impl.PlayerBagImplementation;
 import it.unibo.mparty.model.player.impl.PlayerBuilderImplementation;
 import it.unibo.mparty.model.player.impl.PlayerImplementation;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,6 +31,16 @@ class PlayerBagImplementationTest {
                 .buildPlayer();
         final int num = 3;
         this.bag = new PlayerBagImplementation(num,pl1);
+    }
+
+    /**
+     * Check if an item is correctly added to the bag
+     */
+    @Test
+    public void testAddItem(){
+        final Item item = new ItemFactoryImpl().createItem(ItemName.DADO_FORTUNATO);
+        this.bag.addItem(item);
+        assertTrue(this.bag.getItems().contains(item.getName()));
     }
 
 }
