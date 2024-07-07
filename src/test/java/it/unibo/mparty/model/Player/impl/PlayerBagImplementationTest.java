2fafac9f3e93434159da53ba34c8c975952661e7
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-07 18:25:06 +0200

    Implemented initialisation method in test player bag class

--- src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
+++ src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
@@ -1,6 +1,11 @@
 package it.unibo.mparty.model.Player.impl;
 
+import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.api.PlayerBag;
+import it.unibo.mparty.model.player.impl.PlayerBagImplementation;
+import it.unibo.mparty.model.player.impl.PlayerBuilderImplementation;
+import it.unibo.mparty.model.player.impl.PlayerImplementation;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +16,17 @@ class PlayerBagImplementationTest {
 
     private PlayerBag bag;
 
-
+    /**
+     * Configuration step: this is performed before each test
+     */
+    @BeforeEach
+    public void init(){
+        final Player pl1 = new PlayerBuilderImplementation()
+                .username("user")
+                .character("Mario")
+                .buildPlayer();
+        final int num = 3;
+        this.bag = new PlayerBagImplementation(num,pl1);
+    }
 
 }
