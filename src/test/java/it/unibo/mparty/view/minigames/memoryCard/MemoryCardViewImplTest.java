34741f7b81fb80235af2f820e6e5c90e0c503975
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-07 15:16:13 +0200

    Imported JavaFX testing class and implemented first tests

--- src/test/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImplTest.java
+++ src/test/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImplTest.java
@@ -2,6 +2,36 @@ package it.unibo.mparty.view.minigames.memoryCard;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MemoryCardViewImplTest {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import java.io.IOException;
+
+
+class MemoryCardViewImplTest extends  ApplicationTest{
+
+    private Parent root;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/minigames/memoryCard.fxml"));
+        this.root = loader.load();
+        final Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void testInitialSetup(){
+        final Button button = from(this.root).lookup("#controlButton").query();
+        verifyThat(button, hasText("Start !"));
+    }
 
 }
