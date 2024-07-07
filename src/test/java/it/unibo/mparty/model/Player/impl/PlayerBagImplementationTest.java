6d17c3ea81c6eb6d457d5951bcb3609f0068be91
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-07 18:19:54 +0200

    Initialised Player Bag implementation file

---
+++ src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
@@ -0,0 +1,16 @@
+package it.unibo.mparty.model.Player.impl;
+
+import it.unibo.mparty.model.player.api.PlayerBag;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for a {@link it.unibo.mparty.model.player.impl.PlayerBagImplementation}
+ */
+class PlayerBagImplementationTest {
+
+    private PlayerBag bag;
+
+
+
+}
