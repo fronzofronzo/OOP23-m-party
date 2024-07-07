12158931cf4b160075186e490049fece1a456cd0
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-07 20:52:19 +0200

    Modified default parameters of dice

--- src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
+++ src/main/java/it/unibo/mparty/model/player/impl/DiceImpl.java
@@ -11,8 +11,8 @@ import java.util.Random;
  */
 public class DiceImpl implements Dice {
 
-    private static final int ATTEMPTS = 1;
-    private static final int MAX_NUMBER = 10;
+    private static final int ATTEMPTS = 2;
+    private static final int MAX_NUMBER = 6;
     private static final int MIN_NUMBER = 1;
 
     private int numberOfAttempts = ATTEMPTS;
