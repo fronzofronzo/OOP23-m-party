59625936bf5647961202aad41416163da2ed406b
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-07 15:18:24 +0200

    corrected javadoc, style and final parameters

--- src/main/java/it/unibo/mparty/controller/minigames/connect4/impl/Connect4ControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/connect4/impl/Connect4ControllerImpl.java
@@ -7,31 +7,47 @@ import it.unibo.mparty.model.minigames.connect4.api.Connect4Model;
 import it.unibo.mparty.model.minigames.connect4.impl.Connect4ModelImpl;
 import it.unibo.mparty.view.minigames.connect4.api.Connect4View;
 
+/**
+ * This class implements {@link Connect4Controller} interface.
+ */
 public class Connect4ControllerImpl implements Connect4Controller {
 
     private Connect4View view;
     private final Connect4Model model;
 
-    public Connect4ControllerImpl (Connect4View view) {
-        this.view=view;
-        this.model= new Connect4ModelImpl();
+    /**
+     * Construct a new istance of {@link Connect4ControllerImpl}.
+     * @param view the {@link Connect4View} of the minigame connect4
+     */
+    public Connect4ControllerImpl(final Connect4View view) {
+        this.view = view;
+        this.model = new Connect4ModelImpl();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void endGame() {
         this.view.getMainController().saveMinigameResult(this.model.getResult());
         this.view.getMainView().switchToBoard();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void initGame(List<String> players) {
+    public void initGame(final List<String> players) {
         this.model.setUpPlayers(players);
         this.view.updateDisplayLabel("E' il turno di " + this.model.getPlayer1());
         this.view.activateExitButton(false);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void check(int column) {
+    public void check(final int column) {
         int row = this.model.getAvailableRow(column);
         if (this.model.addDisc(column)) {
             this.view.addCircle(column, row, this.model.getTurnPlayer().equals(this.model.getPlayer1()));
@@ -39,7 +55,7 @@ public class Connect4ControllerImpl implements Connect4Controller {
                 this.view.showResult(this.model.getResult());
                 return;
             }
-            this.view.updateDisplayLabel("E' il turno di "+ this.model.getTurnPlayer());
+            this.view.updateDisplayLabel("E' il turno di " + this.model.getTurnPlayer());
         }
     }
 
