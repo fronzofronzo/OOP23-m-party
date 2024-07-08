39e5b500b338516ebc5cf6a6fc88150e4e1abc57
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-08 11:24:33 +0200

    Modified javadoc of MemoryCardController interface

--- src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memoryCard/MemoryCardController.java
@@ -4,15 +4,17 @@ import it.unibo.mparty.controller.minigames.MinigameController;
 
 /**
  * Interface that models the controller of the minigame Memory card. It
- * represents the Controller of the patter MVC. It
+ * represents the Controller of the patter MVC. It handles the request
+ * made by the user during the game and modify properly the Model. In the case
+ * of Memory Card, in addition to {@link MinigameController} interface methods
+ * add method to handle the selection of a card during the game.
  */
 public interface MemoryCardController extends MinigameController {
 
     /**
-     * Select a certain card
+     * Handles the selection of a card made by the user
      * @param index of the card to select
      */
     void selectCard(int index);
-
 
 }
