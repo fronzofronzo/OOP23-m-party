c5acfdbb82f410bfa4c9a1b74033cff2ec813545
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-08 15:44:36 +0200

    Fixed checkstyles

--- src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
+++ src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
@@ -6,8 +6,6 @@ import it.unibo.mparty.model.item.impl.ItemName;
 import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.api.PlayerBag;
 import it.unibo.mparty.model.player.impl.PlayerBagImplementation;
-import it.unibo.mparty.model.player.impl.PlayerBuilderImplementation;
-import it.unibo.mparty.model.player.impl.PlayerImplementation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -57,7 +55,7 @@ class PlayerBagImplementationTest {
     @Test
     public void testFull() {
         final Item item = new ItemFactoryImpl().createItem(ItemName.DADO_FORTUNATO);
-        for (int i=0; i<capacity; i++){
+        for (int i = 0; i < capacity; i++){
             this.bag.addItem(item);
         }
         assertTrue(this.bag.isFull());
