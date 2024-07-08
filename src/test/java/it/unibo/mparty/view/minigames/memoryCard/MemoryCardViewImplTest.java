5eda3c8ce7a707646ce8bb46cfc83f67e413c377
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-08 11:10:36 +0200

    Added javadoc

--- src/test/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImplTest.java
+++ src/test/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImplTest.java
@@ -14,17 +14,23 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
-
 import org.testfx.framework.junit5.ApplicationExtension;
-
-
 import java.io.IOException;
 
+/**
+ * Test class fot {@link MemoryCardViewImpl} class. Tests that all the visual
+ * elements are correct at start and after some operations.
+ */
 @ExtendWith(ApplicationExtension.class)
 class MemoryCardViewImplTest extends  ApplicationTest{
 
     private Parent root;
 
+    /**
+     * Method to start testing of a JavaFX implementation
+     * @param stage used during testing
+     * @throws IOException if the relative fxml file is not found
+     */
     @Override
     public void start(Stage stage) throws IOException {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/minigames/memoryCard.fxml"));
@@ -34,12 +40,20 @@ class MemoryCardViewImplTest extends  ApplicationTest{
         stage.show();
     }
 
+    /**
+     * Check that the initial setup of the GUI is corrected
+     */
     @Test
     public void testInitialSetup(){
         final Button button = from(this.root).lookup("#controlButton").query();
         verifyThat(button, hasText("Start !"));
     }
 
+    /**
+     * Check that after the first click on the control button, the GUI elements
+     * change properly.
+     * @param robot - will be injected by the test runner
+     */
     @Test
     public void testStartButton(FxRobot robot){
         final Button button = from(this.root).lookup("#controlButton").query();
