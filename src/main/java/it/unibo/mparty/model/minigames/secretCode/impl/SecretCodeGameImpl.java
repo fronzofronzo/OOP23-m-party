5604015474bff95819d06e1b97a6b8e27ff3fc32
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-08 11:32:27 +0200

    Created files about SecreteCodeGame

---
+++ src/main/java/it/unibo/mparty/model/minigames/secretCode/impl/SecretCodeGameImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.mparty.model.minigames.secretCode.impl;
+
+import it.unibo.mparty.model.minigames.secretCode.api.SecretCodeGame;
+
+public class SecretCodeGameImpl implements SecretCodeGame{
+    
+}
