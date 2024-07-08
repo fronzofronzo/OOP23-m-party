164d6f7d408c1a84728e9e3ad2974a7abaf28900
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-08 15:03:45 +0200

    added the construcctor for memory sweep controller

--- src/main/java/it/unibo/mparty/controller/minigames/memorySweep/MemorySweepControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/memorySweep/MemorySweepControllerImpl.java
@@ -1,10 +1,27 @@
 package it.unibo.mparty.controller.minigames.memorySweep;
 
+import it.unibo.mparty.model.minigames.memorysweep.api.MemorySweep;
+import it.unibo.mparty.model.minigames.memorysweep.impl.MemorySweepImpl;
 import it.unibo.mparty.utilities.Position;
+import it.unibo.mparty.view.minigames.memorySweep.MemorySweepView;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 import java.util.List;
 
 public class MemorySweepControllerImpl implements MemorySweepController {
+
+    private MemorySweepView view;
+    private MemorySweep model;
+    private final static int SIZE = 8;
+    private final static int SECONDS = 3000;
+    private final PauseTransition pause = new PauseTransition(new Duration(SECONDS));
+
+    public MemorySweepControllerImpl(MemorySweepView view){
+        this.view = view;
+        this.model = new MemorySweepImpl(SIZE);
+        this.model.setRandomList();
+    }
     @Override
     public void setUp() {
 
