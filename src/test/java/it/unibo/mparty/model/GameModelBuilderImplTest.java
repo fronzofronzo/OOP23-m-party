24b0580bbfabe30e28e95004e57f793a75b3b4ec
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-08 15:31:34 +0200

    Fixed checkstyles

--- src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
+++ src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
@@ -15,7 +15,7 @@ class GameModelBuilderImplTest {
     private GameModelBuilder builder;
 
     /**
-     * Tests initialisation that is performed before each test
+     * Tests initialisation that is performed before each test.
      */
     @BeforeEach
     public void init(){
@@ -23,54 +23,54 @@ class GameModelBuilderImplTest {
     }
 
     /**
-     * Check if the number of minimum and maximum players is handled correctly
+     * Check if the number of minimum and maximum players is handled correctly.
      * by the Game Model builder.
      */
     @Test
-    public void testNumberOfPlayers(){
+    public void testNumberOfPlayers( ){
         final String user1 = "user1";
         final String charact1 = Character.CHAR_MARIO.getName();
-        this.builder = this.builder.addPlayer(user1,charact1);
+        this.builder = this.builder.addPlayer(user1, charact1);
         Assertions.assertFalse(this.builder.enoughPlayers());
         final String user2 = "user2";
         final String charact2 = Character.CHAR_DK.getName();
-        this.builder = this.builder.addPlayer(user2,charact2);
+        this.builder = this.builder.addPlayer(user2, charact2);
         Assertions.assertTrue(this.builder.enoughPlayers());
         final String user3 = "user3";
         final String charact3 = Character.CHAR_PEACH.getName();
-        this.builder = this.builder.addPlayer(user3,charact3);
+        this.builder = this.builder.addPlayer(user3, charact3);
         final String user4 =  "user4";
         final String charact4 = Character.CHAR_YOSHI.getName();
-        this.builder = this.builder.addPlayer(user4,charact4);
+        this.builder = this.builder.addPlayer(user4, charact4);
         Assertions.assertTrue(this.builder.isFull());
     }
 
     /**
-     * Check that adding two players with same username and/or same character
-     * throws {@link IllegalArgumentException}
+     * Check that adding two players with same username and/or same character.
+     * throws {@link IllegalArgumentException}.
      */
     @Test
-    public void testInsertSamePlayer(){
+    public void testInsertSamePlayer() {
         final String user1 = "user1";
         final String charact1 = Character.CHAR_MARIO.getName();
-        this.builder = this.builder.addPlayer(user1,charact1);
+        this.builder = this.builder.addPlayer(user1, charact1);
         final String user2 = "user1";
         final String charact2 = Character.CHAR_MARIO.getName();
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> {this.builder.addPlayer(user2,charact2);});
+                () -> {this.builder.addPlayer(user2, charact2);});
     }
 
     /**
-     * Check if the building of a new GameModel instance is created properly
+     * Check if the building of a new GameModel instance is created properly.
      */
     @Test
-    public void testBuildingModel(){
+    public void testBuildingModel() {
         final String user1 = "user1";
         final String charact1 = Character.CHAR_MARIO.getName();
         final String user2 = "user2";
         final String charact2 = Character.CHAR_DK.getName();
-        final GameModel model = this.builder.addPlayer(user1,charact1)
-                .addPlayer(user2,charact2)
+        final GameModel model = this.builder.addPlayer(user1, charact1)
+                .addPlayer(user2, charact2)
                 .difficulty(BoardType.EASY.toString()).build();
         Assertions.assertTrue(model.getPlayers().stream()
                 .map(Player::getUsername)
--- src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
+++ src/test/java/it/unibo/mparty/model/Player/impl/PlayerBagImplementationTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Test class for a {@link it.unibo.mparty.model.player.impl.PlayerBagImplementation}
+ * Test class for a {@link it.unibo.mparty.model.player.impl.PlayerBagImplementation}.
  */
 class PlayerBagImplementationTest {
 
@@ -22,29 +22,29 @@ class PlayerBagImplementationTest {
     private int capacity;
 
     /**
-     * Configuration step: this is performed before each test
+     * Configuration step: this is performed before each test.
      */
     @BeforeEach
-    public void init(){
+    public void init() {
         capacity =  3;
         this.bag = new PlayerBagImplementation(capacity);
     }
 
     /**
-     * Check if an item is correctly added to the bag
+     * Check if an item is correctly added to the bag.
      */
     @Test
-    public void testAddItem(){
+    public void testAddItem() {
         final Item item = new ItemFactoryImpl().createItem(ItemName.DADO_FORTUNATO);
         this.bag.addItem(item);
         assertTrue(this.bag.getItems().contains(item.getName()));
     }
 
     /**
-     * Check if an item is correctly removed from the bag
+     * Check if an item is correctly removed from the bag.
      */
     @Test
-    public void testRemoveItem(){
+    public void testRemoveItem() {
         final Item item = new ItemFactoryImpl().createItem(ItemName.DADO_FORTUNATO);
         this.bag.addItem(item);
         assertEquals(item, this.bag.useItem(item.getName()));
@@ -52,10 +52,10 @@ class PlayerBagImplementationTest {
     }
 
     /**
-     * Check if the {@code isFull} function works properly
+     * Check if the {@code isFull} function works properly.
      */
     @Test
-    public void testFull(){
+    public void testFull() {
         final Item item = new ItemFactoryImpl().createItem(ItemName.DADO_FORTUNATO);
         for (int i=0; i<capacity; i++){
             this.bag.addItem(item);
