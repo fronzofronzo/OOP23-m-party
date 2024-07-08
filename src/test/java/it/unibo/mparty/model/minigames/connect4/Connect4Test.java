6f45868bcd2c289f7debe15dc55ef232551af56e
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-08 11:47:27 +0200

    update test connect4

--- src/test/java/it/unibo/mparty/model/minigames/connect4/Connect4Test.java
+++ src/test/java/it/unibo/mparty/model/minigames/connect4/Connect4Test.java
@@ -1,5 +1,33 @@
 package it.unibo.mparty.model.minigames.connect4;
 
-public class Connect4Test {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.mparty.model.minigames.connect4.api.Connect4Model;
+import it.unibo.mparty.model.minigames.connect4.impl.Connect4ModelImpl;
+
+/**
+ * This class tests the {@link Connect4ModelImpl} class.
+ */
+public class Connect4Test {
+    private Connect4Model testModel;
+
+    /**
+     * Initialize the game before each test.
+     */
+    @BeforeEach
+    public void initGame() {
+        testModel = new Connect4ModelImpl();
+        testModel.setUpPlayers(List.of("testPlayer1", "testPlayer2"));
+    }
+
+    @Test
+    public void testAddDisc() {
+        assertTrue(testModel.addDisc(2));
+        assertTrue(testModel.addDisc(2));
+    }
 }
