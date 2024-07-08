2c1e0914e0467a9bbf245df3fcaef59c7f9b670c
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-08 15:27:59 +0200

    Fixed checkstyles

--- src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
+++ src/test/java/it/unibo/mparty/model/GameModelBuilderImplTest.java
@@ -2,12 +2,10 @@ package it.unibo.mparty.model;
 
 import it.unibo.mparty.model.player.api.Player;
 import it.unibo.mparty.model.player.impl.Character;
-import it.unibo.mparty.model.player.impl.PlayerImplementation;
 import it.unibo.mparty.utilities.BoardType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * Test class for {@link GameModelBuilderImpl} class
@@ -33,18 +31,18 @@ class GameModelBuilderImplTest {
         final String user1 = "user1";
         final String charact1 = Character.CHAR_MARIO.getName();
         this.builder = this.builder.addPlayer(user1,charact1);
-        assertFalse(this.builder.enoughPlayers());
+        Assertions.assertFalse(this.builder.enoughPlayers());
         final String user2 = "user2";
         final String charact2 = Character.CHAR_DK.getName();
         this.builder = this.builder.addPlayer(user2,charact2);
-        assertTrue(this.builder.enoughPlayers());
+        Assertions.assertTrue(this.builder.enoughPlayers());
         final String user3 = "user3";
         final String charact3 = Character.CHAR_PEACH.getName();
         this.builder = this.builder.addPlayer(user3,charact3);
         final String user4 =  "user4";
         final String charact4 = Character.CHAR_YOSHI.getName();
         this.builder = this.builder.addPlayer(user4,charact4);
-        assertTrue(this.builder.isFull());
+        Assertions.assertTrue(this.builder.isFull());
     }
 
     /**
@@ -58,7 +56,7 @@ class GameModelBuilderImplTest {
         this.builder = this.builder.addPlayer(user1,charact1);
         final String user2 = "user1";
         final String charact2 = Character.CHAR_MARIO.getName();
-        assertThrows(IllegalArgumentException.class,
+        Assertions.assertThrows(IllegalArgumentException.class,
                 () -> {this.builder.addPlayer(user2,charact2);});
     }
 
@@ -74,10 +72,10 @@ class GameModelBuilderImplTest {
         final GameModel model = this.builder.addPlayer(user1,charact1)
                 .addPlayer(user2,charact2)
                 .difficulty(BoardType.EASY.toString()).build();
-        assertTrue(model.getPlayers().stream()
+        Assertions.assertTrue(model.getPlayers().stream()
                 .map(Player::getUsername)
                 .toList().contains(user1));
-        assertTrue(model.getPlayers().stream()
+        Assertions.assertTrue(model.getPlayers().stream()
                 .map(Player::getUsername)
                 .toList().contains(user2));
     }
