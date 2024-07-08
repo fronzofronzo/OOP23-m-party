85f71d381cf12b19b4beb2d617615af6278d96ca
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-08 11:05:22 +0200

    Implemented test to start of the view

--- src/test/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImplTest.java
+++ src/test/java/it/unibo/mparty/view/minigames/memoryCard/MemoryCardViewImplTest.java
@@ -6,15 +6,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
+import org.testfx.framework.junit5.ApplicationExtension;
+
+
 import java.io.IOException;
 
-
+@ExtendWith(ApplicationExtension.class)
 class MemoryCardViewImplTest extends  ApplicationTest{
 
     private Parent root;
@@ -32,6 +38,15 @@ class MemoryCardViewImplTest extends  ApplicationTest{
     public void testInitialSetup(){
         final Button button = from(this.root).lookup("#controlButton").query();
         verifyThat(button, hasText("Start !"));
+    }
+
+    @Test
+    public void testStartButton(FxRobot robot){
+        final Button button = from(this.root).lookup("#controlButton").query();
+        final Label textLabel = from(this.root).lookup("#textLabel").query();
+        robot.clickOn(button);
+        verifyThat(button, hasText("Pronto !"));
+        verifyThat(textLabel, hasText("Quando si e' pronti, spingere il pulsante 'Pronto' "));
     }
 
 }
