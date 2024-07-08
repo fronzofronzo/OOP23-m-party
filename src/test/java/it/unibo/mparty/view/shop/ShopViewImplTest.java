7630e976f4fa9e712e3a795ac2948817879ca958
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2024-07-08 16:13:43 +0200

    implemented test for shopView

---
+++ src/test/java/it/unibo/mparty/view/shop/ShopViewImplTest.java
@@ -0,0 +1,42 @@
+package it.unibo.mparty.view.shop;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import it.unibo.mparty.view.shop.impl.ShopViewImpl;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * This class tests the {@link ShopViewImpl} class.
+ */
+@ExtendWith(ApplicationExtension.class)
+public class ShopViewImplTest extends ApplicationTest {
+    private Parent testRoot;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Shop.fxml"));
+        this.testRoot = loader.load();
+        final Scene scene = new Scene (testRoot);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void should_contain_exitButton() {
+        verifyThat(".button", hasText("Esci"));
+    }
+
+    @Test
+    public void should_contain_label() {
+        verifyThat(".label", hasText("Seleziona l'Item che desideri acquistare"));
+    }
+}
