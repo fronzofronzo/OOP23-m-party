b3b7bdbd515717b68480941e55e247ee139afb0a
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2024-07-09 22:30:43 +0200

    created fxml files and classes for tutorial screen

---
+++ src/main/java/it/unibo/mparty/view/InitialScreen/impl/TutorialScreenImpl.java
@@ -0,0 +1,21 @@
+package it.unibo.mparty.view.InitialScreen.impl;
+
+import it.unibo.mparty.view.InitialScreen.api.TutorialScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TutorialScreenImpl implements TutorialScreen, Initializable {
+
+    @Override
+    public void handleButton(ActionEvent e) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
