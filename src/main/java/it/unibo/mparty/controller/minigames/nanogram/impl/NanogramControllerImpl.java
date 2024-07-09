50a341e2fc47b7195ba2f6e30f4d86203aef2833
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-09 08:55:07 +0200

    fixed nanogram checkStyle

--- src/main/java/it/unibo/mparty/controller/minigames/nanogram/impl/NanogramControllerImpl.java
+++ src/main/java/it/unibo/mparty/controller/minigames/nanogram/impl/NanogramControllerImpl.java
@@ -2,7 +2,6 @@ package it.unibo.mparty.controller.minigames.nanogram.impl;
 
 import it.unibo.mparty.controller.minigames.nanogram.api.NanogramController;
 import it.unibo.mparty.model.minigames.nanogram.impl.NanogramModelImpl;
-import it.unibo.mparty.view.minigames.nanogram.NanogramMessage;
 import it.unibo.mparty.view.minigames.nanogram.impl.NanogramViewImpl;
 
 import java.util.List;
