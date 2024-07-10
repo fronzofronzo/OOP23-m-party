e6ac95c4c96a96a2731456d25e2d7a14aa158eb5
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 01:02:48 +0200

    Added java doc and exception in RandomFromSet

--- src/main/java/it/unibo/mparty/utilities/RandomFromSet.java
+++ src/main/java/it/unibo/mparty/utilities/RandomFromSet.java
@@ -3,9 +3,23 @@ package it.unibo.mparty.utilities;
 import java.util.Set;
 import java.util.Random;
 
+/**
+ * The class RandomFromSet return a random value from a set
+ */
 public class RandomFromSet {
 
+    private static final String ERROR_MESSAGE = "Il set in input non deve essere vuoto";
+
+    /**
+     * Return a random value form the input set
+     * @param inputSet the input set
+     * @return a random value form the input set
+     * @throws {@link IllegalArgumentException} if the input set is empty
+     */
     public static <E> E get(Set<E> inputSet) {
+        if (inputSet.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
         return inputSet.stream().skip(new Random().nextInt(inputSet.size())).findFirst().get();
     }
 }
