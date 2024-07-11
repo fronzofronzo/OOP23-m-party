87fce16c840669cab9af296f8a3755f949581abb
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 12:26:12 +0200

    Wrote java doc in enum in SecreteCodeModel

--- src/main/java/it/unibo/mparty/model/minigames/secretCode/util/SecretCodeResults.java
+++ src/main/java/it/unibo/mparty/model/minigames/secretCode/util/SecretCodeResults.java
@@ -1,7 +1,20 @@
 package it.unibo.mparty.model.minigames.secretCode.util;
 
+/**
+ * This enum SecretCodeResults indicate the result type of a guess in the game
+ * Secrete Code.
+ */
 public enum SecretCodeResults {
+    /**
+     * 
+     */
     CORRECT_COLOR_AND_POSITION,
+    /**
+     * 
+     */
     CORRECT_COLOR,
-    WRONG_COLOR
+    /**
+     * 
+     */
+    WRONG_COLOR;
 }
