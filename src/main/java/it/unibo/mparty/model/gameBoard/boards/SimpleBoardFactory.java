326bcd60ee7e85582f1915163d550e6c1fd94448
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 02:42:55 +0200

    Wrote java doc in SimpleBoardFactory

--- src/main/java/it/unibo/mparty/model/gameBoard/boards/SimpleBoardFactory.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/boards/SimpleBoardFactory.java
@@ -4,16 +4,21 @@ import it.unibo.mparty.model.gameBoard.api.GameBoard;
 import it.unibo.mparty.utilities.BoardType;
 
 /**
- * This class is a factory that creates a board based on a selection
+ * This class is a factory that creates a {@link GameBoard} based on the selected {@link BoardType}.
  */
 public class SimpleBoardFactory {
 
+    /**
+     * Return the {@link GameBoard} based on the selected {@link BoardType}.
+     * @param boardSelected the idicates the {@link BoardType}
+     * @return the {@link GameBoard}.
+     */
     public GameBoard createBoard(BoardType boardSelected) {
         switch (boardSelected) {
             case EASY: return new EasyGameBoard();
             case MEDIUM: return new MediumGameBoard();
             case HARD: return new HardGameBoard();
-            default: return new EasyGameBoard();
+            default: throw new IllegalStateException();
         }
     }    
 }
