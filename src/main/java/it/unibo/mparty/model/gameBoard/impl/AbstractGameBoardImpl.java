bd03a578603484426cec7b65f8748e6bff202403
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 20:38:31 +0200

    Removed unuded import in AbstractGameBoardImpl

--- src/main/java/it/unibo/mparty/model/gameBoard/impl/AbstractGameBoardImpl.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/impl/AbstractGameBoardImpl.java
@@ -14,7 +14,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 import it.unibo.mparty.model.gameboard.api.GameBoard;
 import it.unibo.mparty.model.gameboard.api.Slot;
