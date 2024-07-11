86e0f14b36d40b686d21fe7c1e94af8e418a39dc
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 12:35:16 +0200

    Java doc and check style for SecreCodePlayer and implementation

--- src/main/java/it/unibo/mparty/model/minigames/secretCode/impl/SecretCodePlayerImpl.java
+++ src/main/java/it/unibo/mparty/model/minigames/secretCode/impl/SecretCodePlayerImpl.java
@@ -7,17 +7,25 @@ import java.util.List;
 import it.unibo.mparty.model.minigames.secretCode.api.SecretCodePlayer;
 import it.unibo.mparty.model.minigames.secretCode.util.SecretCodeColors;
 
-public class SecretCodePlayerImpl implements SecretCodePlayer{
+/**
+ * This class implementes {@link SecreteCodePlayer}.
+ */
+public class SecretCodePlayerImpl implements SecretCodePlayer {
 
-    private static final  int INITIAL_POINTS = 0;
-
+    private static final int INITIAL_POINTS = 0;
     private final String player;
     private final int soluctionSize;
     private List<SecretCodeColors> myGuess;
     private int points;
 
-
-    public SecretCodePlayerImpl(String player, int soluctionSize){
+    /**
+     * This is the constructor of a {@link SecreteCodePlayerImpl} that is identified
+     * by a name and it needs the soluction's size in order to create his guesses.
+     * 
+     * @param player        the name of the player.
+     * @param soluctionSize the size of the soluction that the player have to guess.
+     */
+    public SecretCodePlayerImpl(String player, int soluctionSize) {
         this.player = player;
         this.soluctionSize = soluctionSize;
         this.myGuess = new ArrayList<>();
@@ -25,7 +33,6 @@ public class SecretCodePlayerImpl implements SecretCodePlayer{
     }
 
     /**
-     *
      * {@inheritDoc}
      */
     @Override
@@ -38,34 +45,6 @@ public class SecretCodePlayerImpl implements SecretCodePlayer{
     }
 
     /**
-     *
-     * {@inheritDoc}
-     */
-    @Override
-    public List<SecretCodeColors> getGuess() {
-        return Collections.unmodifiableList(this.myGuess);
-    }
-
-    /**
-     *
-     * {@inheritDoc}
-     */
-    @Override
-    public String getNamePlayer() {
-        return this.player;
-    }
-
-    /**
-     *
-     * {@inheritDoc}
-     */
-    @Override
-    public int getPoints() {
-        return this.points;
-    }
-
-    /**
-     *
      * {@inheritDoc}
      */
     @Override
@@ -74,11 +53,34 @@ public class SecretCodePlayerImpl implements SecretCodePlayer{
     }
 
     /**
-     *
      * {@inheritDoc}
      */
     @Override
     public void startNewGuess() {
         this.myGuess = new ArrayList<>();
-    }    
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getNamePlayer() {
+        return this.player;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<SecretCodeColors> getGuess() {
+        return Collections.unmodifiableList(this.myGuess);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getPoints() {
+        return this.points;
+    }
 }
