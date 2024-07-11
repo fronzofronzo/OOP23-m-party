368a94b2f8db40dc695800be6bdc209d3f16cab9
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 18:13:15 +0200

    Created SecreteCodeViewImplTest

---
+++ src/test/java/it/unibo/mparty/view/minigames/secreteCode/SecreteCodeViewImplTest.java
@@ -0,0 +1,25 @@
+package it.unibo.mparty.view.minigames.secreteCode;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+class SecreteCodeViewImplTest extends ApplicationTest{
+    
+    private Parent root;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/minigames/secretCode.fxml"));
+        this.root = loader.load();
+        final Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
