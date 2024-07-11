3598842b14b5ba06031a938f9fa32b94d62313e9
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 02:51:11 +0200

    Wrote java doc for all boards game types

--- src/main/java/it/unibo/mparty/model/gameBoard/boards/HardGameBoard.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/boards/HardGameBoard.java
@@ -1,5 +1,6 @@
 package it.unibo.mparty.model.gameBoard.boards;
 
+import it.unibo.mparty.model.gameBoard.api.GameBoard;
 import it.unibo.mparty.model.gameBoard.impl.AbstractBoardImpl;
 import it.unibo.mparty.utilities.BoardType;
 import it.unibo.mparty.utilities.Position;
@@ -8,38 +9,44 @@ import it.unibo.mparty.utilities.SlotType;
 import java.util.Map;
 import java.util.Set;
 
-public class HardGameBoard extends AbstractBoardImpl{
+/**
+ * The class EasyGameBoard models a {@link GameBoard} that correspond to the
+ * {@link BoardType.HARD}.
+ */
+public class HardGameBoard extends AbstractBoardImpl {
 
-    private final static BoardType BOARD_TYPE = BoardType.HARD; 
-
+    private final static BoardType BOARD_TYPE = BoardType.HARD;
     private static final int WIDTH = 30;
     private static final int HEIGHT = 20;
-
     private static final int INITIAL_X_HARD_BOARD = 28;
     private static final int INITIAL_Y_HARD_BOARD = 14;
-
     private static final int PROB_PATH = 15;
     private static final int PROB_SINGLEPLAYER = 20;
     private static final int PROB_MALUS = 20;
     private static final int PROB_MULTIPLAYER = 20;
     private static final int PROB_SHOP = 10;
     private static final int PROB_BONUS = 15;
-
     private static final String FILE_PATH = "HardGameBoard.txt";
+    private static final Map<SlotType, Integer> RULES = Map.of(SlotType.PATH, PROB_PATH,
+            SlotType.SINGLEPLAYER, PROB_SINGLEPLAYER,
+            SlotType.MALUS, PROB_MALUS,
+            SlotType.MULTIPLAYER, PROB_MULTIPLAYER,
+            SlotType.SHOP, PROB_SHOP,
+            SlotType.BONUS, PROB_BONUS);
+    private static final Set<Position> STARS_POSITIONS = Set.of(new Position(19, 12),
+            new Position(12, 1),
+            new Position(7, 18));
 
-    private static final Map<SlotType,Integer> RULES = 
-        Map.of(SlotType.PATH, PROB_PATH,
-               SlotType.SINGLEPLAYER, PROB_SINGLEPLAYER,
-               SlotType.MALUS, PROB_MALUS,
-               SlotType.MULTIPLAYER, PROB_MULTIPLAYER,
-               SlotType.SHOP, PROB_SHOP,
-               SlotType.BONUS, PROB_BONUS);
-
-    private static final Set<Position> STARS_POSITIONS = Set.of(new Position(19, 12), 
-                                                                new Position(12, 1),
-                                                                new Position(7, 18));
-           
+    /**
+     * This is the constructor of the HardGameBoard.
+     */
     public HardGameBoard() {
-        super(WIDTH, HEIGHT, new Position(INITIAL_X_HARD_BOARD, INITIAL_Y_HARD_BOARD), STARS_POSITIONS, FILE_PATH, RULES, BOARD_TYPE);
+        super(WIDTH,
+                HEIGHT,
+                new Position(INITIAL_X_HARD_BOARD, INITIAL_Y_HARD_BOARD),
+                STARS_POSITIONS,
+                FILE_PATH,
+                RULES,
+                BOARD_TYPE);
     }
 }
