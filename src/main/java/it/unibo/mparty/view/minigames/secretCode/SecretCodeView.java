9f644065e6987ec7fabef7351b70254ae61ca279
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 15:08:58 +0200

    Corrected view of secret code

--- src/main/java/it/unibo/mparty/view/minigames/secretCode/SecretCodeView.java
+++ src/main/java/it/unibo/mparty/view/minigames/secretCode/SecretCodeView.java
@@ -4,14 +4,37 @@ import java.util.List;
 
 import it.unibo.mparty.model.minigames.secretCode.util.SecretCodeColors;
 import it.unibo.mparty.model.minigames.secretCode.util.SecretCodeResults;
-import it.unibo.mparty.utilities.Position;
+import it.unibo.mparty.utilities.Pair;
 import it.unibo.mparty.view.minigames.MinigameView;
 
-public interface SecretCodeView extends MinigameView{
+/**
+ * This class model the view of the game Secret Code based on the pattern MVC
+ * and extends {@link MinigameView}.
+ */
+public interface SecretCodeView extends MinigameView {
 
-    void updateGuesses(String player, Position pos, SecretCodeColors guess);
+    /**
+     * Update the guess of the input player.
+     * 
+     * @param player the player to be updated.
+     * @param pos    a {@link Pair} that indicates which guess has to be updated.
+     * @param guess  the {@link SecretCodeColors} of the guess.
+     */
+    void updateGuess(String player, Pair<Integer, Integer> pos, SecretCodeColors guess);
 
+    /**
+     * Update the result of a guess.
+     * 
+     * @param player  the player to be updated.
+     * @param turn    the current turn.
+     * @param results the {@link SecretCodeResults}.
+     */
     void updateResults(String player, int turn, List<SecretCodeResults> results);
-    
+
+    /**
+     * Show the soluction of the game.
+     * 
+     * @param solution the soluction.
+     */
     void showSolution(List<SecretCodeColors> solution);
 }
