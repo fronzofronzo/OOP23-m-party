a6077d7787596f59bc5462574027796c93c8ef61
RicciNicholas <nicholas.ricci3@studio.unibo.it>
2024-07-11 02:43:27 +0200

    Corrected SimpleBoardFactory

--- src/main/java/it/unibo/mparty/model/gameBoard/boards/SimpleBoardFactory.java
+++ src/main/java/it/unibo/mparty/model/gameBoard/boards/SimpleBoardFactory.java
@@ -4,21 +4,27 @@ import it.unibo.mparty.model.gameBoard.api.GameBoard;
 import it.unibo.mparty.utilities.BoardType;
 
 /**
- * This class is a factory that creates a {@link GameBoard} based on the selected {@link BoardType}.
+ * This class is a factory that creates a {@link GameBoard} based on the
+ * selected {@link BoardType}.
  */
 public class SimpleBoardFactory {
 
     /**
      * Return the {@link GameBoard} based on the selected {@link BoardType}.
+     * 
      * @param boardSelected the idicates the {@link BoardType}
      * @return the {@link GameBoard}.
      */
     public GameBoard createBoard(BoardType boardSelected) {
         switch (boardSelected) {
-            case EASY: return new EasyGameBoard();
-            case MEDIUM: return new MediumGameBoard();
-            case HARD: return new HardGameBoard();
-            default: throw new IllegalStateException();
+            case EASY:
+                return new EasyGameBoard();
+            case MEDIUM:
+                return new MediumGameBoard();
+            case HARD:
+                return new HardGameBoard();
+            default:
+                throw new IllegalStateException();
         }
-    }    
+    }
 }
