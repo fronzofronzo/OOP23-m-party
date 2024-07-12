988ffd8c5e10a4333357dab9d73bedc094f14c3b
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-12 17:34:08 +0200

    Modified testButton test

--- src/test/java/it/unibo/mparty/view/minigames/memorycard/MemoryCardViewImplTest.java
+++ src/test/java/it/unibo/mparty/view/minigames/memorycard/MemoryCardViewImplTest.java
@@ -1,11 +1,15 @@
 package it.unibo.mparty.view.minigames.memorycard;
 
+import it.unibo.mparty.view.minigames.MinigameView;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
@@ -17,6 +21,7 @@ import static org.testfx.matcher.control.LabeledMatchers.hasText;
 import org.testfx.framework.junit5.ApplicationExtension;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Test class fot {@link MemoryCardViewImpl} class. Tests that all the visual
@@ -37,6 +42,8 @@ class MemoryCardViewImplTest extends ApplicationTest {
     public void start(final Stage stage) throws IOException {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/minigames/memoryCard.fxml"));
         this.root = loader.load();
+        final MinigameView controller = loader.getController();
+        controller.startMinigame(List.of("user1"));
         final Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -48,22 +55,25 @@ class MemoryCardViewImplTest extends ApplicationTest {
     @Test
     void testInitialSetup() {
         final Button button = from(this.root).lookup("#controlButton").query();
-        final Label label = from(this.root).lookup("#textLabel").query();
         verifyThat(button, hasText("Start !"));
-        verifyThat(label, hasText(""));
     }
 
     /**
-     * Check that the button works properly.
+     * Check that the start button works properly.
      * @param robot used to test.
      */
     @Test
     void testButton(final FxRobot robot) {
         final Button button = from(this.root).lookup("#controlButton").query();
         final Label label = from(this.root).lookup("#textLabel").query();
+        final FlowPane pane = from(this.root).lookup("#cardsPane").query();
         robot.clickOn(button);
         verifyThat(button, hasText("Pronto !"));
         verifyThat(label, hasText("Quando si e' pronti, spingere il pulsante 'Pronto'"));
+        Assertions.assertFalse(pane.getChildren().isEmpty());
+        for (final Node n : pane.getChildren()) {
+            Assertions.assertTrue(n.isDisable());
+        }
     }
 
 }
