e4b401214b3fe8ebbcc3efdc0f92af99cfa15610
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-12 21:27:09 +0200

    Modified build gradle kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     val jUnitVersion = "5.10.2"
     // JUnit API and testing engine
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
-    testImplementation("com.github.spotbugs:spotbugs-annotations:4.7.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 
     val testFxVersion = "4.0.16-alpha"
