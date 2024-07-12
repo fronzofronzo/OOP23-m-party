ec94996e46d65fb237adf0680a4cf5b45ec89ee0
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-12 18:47:36 +0200

    removed useless method

--- src/test/java/it/unibo/mparty/model/minigames/nanogram/LiveImplTest.java
+++ src/test/java/it/unibo/mparty/model/minigames/nanogram/LiveImplTest.java
@@ -5,7 +5,6 @@ import it.unibo.mparty.model.minigames.nanogram.live.impl.LiveImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -14,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
  */
 class LiveImplTest {
 
-    private static final int INITIAL_LIVES = 3;
     private LiveImpl live;
 
     /**
@@ -23,18 +21,6 @@ class LiveImplTest {
     @BeforeEach
     public void setUp() {
         this.live = new LiveImpl();
-    }
-
-    /**
-     * Tests the {@code reset} method.
-     * Ensures that the lives are reset to the initial value.
-     */
-    @Test
-    @SuppressFBWarnings(value = "UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR",
-            justification = "The field 'live' is initialized in the setUp method, which is run before each test.")
-    void reset() {
-        this.live.reset();
-        assertEquals(INITIAL_LIVES, this.live.getLive());
     }
 
     /**
