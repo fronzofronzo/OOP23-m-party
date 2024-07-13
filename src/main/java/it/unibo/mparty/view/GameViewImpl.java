15176493d317828ed6eb70035843abed91c91f50
Gabriele <gabriele.fronzoni@studio.unibo.it>
2024-07-13 17:35:44 +0200

    Removed test code

--- src/main/java/it/unibo/mparty/view/GameViewImpl.java
+++ src/main/java/it/unibo/mparty/view/GameViewImpl.java
@@ -54,7 +54,6 @@ public class GameViewImpl extends Application implements GameView {
         this.setBoardView();
         final Pair<Scene, SceneView> scenePair = this.loadScene("InitialScreen");
         this.stage.setScene(scenePair.getFirst());
-        //this.setMinigameScene("memoryCard", List.of("player1", "player2"));
         this.stage.setMinWidth(PREF_WIDTH);
         this.stage.setMinHeight(PREF_HEIGHT);
         this.stage.setMaximized(true);
